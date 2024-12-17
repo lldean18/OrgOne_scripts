73776f2d93bd8bd26fbd2378ecdfdbcef9eb8767
Laura Dean <lldean18@gmail.com>
2024-12-17 12:07:19 +0000

    The newest hifiasm is just not working on the concatenated fastq.gz file of ALL simplex reads - im getting a malloc() memory error but wondering if the files I'm cat'ing together are differentially compressed and that's causing problems so concatenting them with zcat and recompressing them. The will check the md5 sums to ensure the 2 concatenated fastq.gz files are identical to ensure that isn't why hifiasm is erroring out

---
+++ sumatran_tiger/07.8.2_tig_cat_fastqs.bash
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Laura Dean
+# 17/12/24
+# for running on Ada
+
+#SBATCH --partition=defq
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=100g
+#SBATCH --time=36:00:00
+#SBATCH --job-name=tig_merge_fqs
+#SBATCH --output=/gpfs01/home/mbzlld/code_and_scripts/slurm_out_scripts/slurm-%x-%j.out
+
+
+
+
+zcat \
+/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/basecalls/all_simplex_simplex.fastq.gz \
+/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/basecalls/sumatran_tiger_duplex/20230221_1715_2D_PAK99090_77cd6ba1/pod5_fail/SUPlatest_calls_simplex.fastq.gz \
+/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/basecalls/sumatran_tiger_duplex/20230221_1715_2D_PAK99090_77cd6ba1/pod5_pass/SUPlatest_calls_simplex.fastq.gz \
+/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/basecalls/sumatran_tiger_duplex_ns/20230223_1304_2D_PAK99090_d47818d2/pod5_fail/SUPlatest_calls_simplex.fastq.gz \
+/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/basecalls/sumatran_tiger_duplex_ns/20230223_1304_2D_PAK99090_d47818d2/pod5_pass/SUPlatest_calls_simplex.fastq.gz \
+/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/basecalls/sumatran_tiger_duplex_ns/20230222_1213_2D_PAK99090_a9de44bc/pod5_fail/SUPlatest_calls_simplex.fastq.gz \
+/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/basecalls/sumatran_tiger_duplex_ns/20230222_1213_2D_PAK99090_a9de44bc/pod5_pass/SUPlatest_calls_simplex.fastq.gz \
+/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/basecalls/sumatran_tiger_duplex_P2/20230307_1702_1G_PAK99084_16f44a88/pod5_fail/SUPlatest_calls_simplex.fastq.gz \
+/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/basecalls/sumatran_tiger_duplex_P2/20230307_1702_1G_PAK99084_16f44a88/pod5_pass/SUPlatest_calls_simplex.fastq.gz \
+/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/basecalls/sumatran_tiger_duplex_P3/20230309_1159_2G_PAK98873_d39c7c8c/pod5_fail/SUPlatest_calls_simplex.fastq.gz \
+/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/basecalls/sumatran_tiger_duplex_P3/20230309_1159_2G_PAK98873_d39c7c8c/pod5_pass/SUPlatest_calls_simplex.fastq.gz | gzip > /gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/basecalls/ALL_simplex.fastq.gz
+
+
+
