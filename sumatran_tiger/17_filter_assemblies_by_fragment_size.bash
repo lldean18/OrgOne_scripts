a98ce457ef4c6ffe36eb58e66ee05379f1d429b3
Laura Dean <lldean18@gmail.com>
2025-01-09 10:04:06 +0000

    updating script for assembly fragment size filtering to run on another assembly - it also now filters both the fasta and GFA files

--- sumatran_tiger/17_filter_assemblies_by_fragment_size.bash
+++ sumatran_tiger/17_filter_assemblies_by_fragment_size.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Laura Dean
-# 3/1/25
+# 9/1/25
 # For running on the UoN HPC Ada
 
 #SBATCH --partition=defq
@@ -20,11 +20,12 @@ conda activate seqtk
 
 
 # set variables
-assembly=/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/hifiasm_asm9/ONTasm.bp.p_ctg
+#assembly=/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/hifiasm_asm9/ONTasm.bp.p_ctg
+assembly=/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/hifiasm_asm10/ONTasm.bp.p_ctg
 
 
 # remove sequences shorter than 100kb
-#seqtk seq -L 100000 $assembly.fasta > ${assembly}_100kb.fasta
+seqtk seq -L 100000 $assembly.fasta > ${assembly}_100kb.fasta
 
 
 # unload software
