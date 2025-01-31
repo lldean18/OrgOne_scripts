8813cd6638db3dc110e2c0d5da7f0862c73849b2
Laura Dean <lldean18@gmail.com>
2025-01-31 15:36:59 +0000

    updated version of plotsr script with more of it working

--- sumatran_tiger/21_plotsr_genome_comparison.bash
+++ sumatran_tiger/21_plotsr_genome_comparison.bash
@@ -22,80 +22,59 @@ asm1=/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/hifiasm_asm10/ONTasm.bp.p_ct
 source $HOME/.bash_profile
 module load samtools-uoneasy/1.18-GCC-12.3.0
 
-# assign our fragments to chromosomes
-# index the reference assembly
-conda create --name bioawk bioawk -y
-conda activate bioawk
-bioawk -c fastx '{print $name ":" 1 "-" length($seq)}' $reference > ${reference%.*}regions_file.txt
+#############################################
+#### assign our fragments to chromosomes ####
+#############################################
+
+# # generate a regions file for filtering
+# #conda create --name bioawk bioawk -y
+# conda activate bioawk
+# bioawk -c fastx '{print $name ":" 1 "-" length($seq)}' $reference > ${reference%.*}_regions_file.txt
+# conda deactivate
+
+# # align our assembly to the reference
+# conda activate minimap2
+# minimap2 -x asm5 -t 16 $reference $asm1 > ${asm1%.*}_alignment.paf
+# conda deactivate
+
+# # identify the best matching chromosomes for each of our fragments
+# awk '{print $1, $6, $8-$7}' ${asm1%.*}_alignment.paf | sort -k1,1 -k3nr | awk '!seen[$1]++' > ${asm1%.*}_contig_assignments.txt
+# sed -i -r 's/[^ ]*$//' ${asm1%.*}_contig_assignments.txt # get rid of the contig lengths at the ends of the lines
+# sed -i -r 's/ /\t/' ${asm1%.*}_contig_assignments.txt # replace spaces with tabs
+
+# # rename contigs in our assembly based on their assignments
+# conda activate seqkit
+# seqkit replace -p "^(.*)" -r "{kv}" --kv-file ${asm1%.*}_contig_assignments.txt $asm1 > ${asm1%.*}_ref_renamed_contigs.fasta
+# conda deactivate
+
+###########################################
+#### map our assembly to the reference ####
+###########################################
+
+conda activate minimap2
+
+# align the genomes
+#asm=asm5
+asm=asm10
+#asm=asm20
+
+# should also try -asm10 and -asm20 for up to 1% / 5% sequence divergence
+minimap2 \
+-ax $asm \
+-t 16 \
+--eqx $asm1 $reference \
+-o $wkdir/tmp.sam
+samtools sort $wkdir/tmp.sam \
+-o $wkdir/$(basename ${asm%.*})_$asm.bam
+rm $wkdir/tmp.sam
 conda deactivate
 
+# write the names of the assemblies to a file for use by plotsr
+echo -e ""$asm1"\tHifiasm10
+"$reference"\tReference" > $wkdir/$(basename ${asm1%.*})_plotsr_assemblies_list.txt
 
+module unload samtools-uoneasy/1.12-GCC-9.3.0
 
-
-# The chromosome names have to match across files
-
-## remove the _RagTag suffix from the chromosome names in the fasta files
-#sed 's/_RagTag//' /gpfs01/share/StickleAss/plotsr/marine_Duin.fasta > /gpfs01/share/StickleAss/plotsr/Duin_clean.fasta
-#sed 's/_RagTag//' /gpfs01/share/StickleAss/plotsr/freshwater_Scad.fasta > /gpfs01/share/StickleAss/plotsr/Scad_clean.fasta
-
-
-
-## Remove all of the scaffolds from the fastq files that are still not assigned to chromosomes with samtools faidx
-## Duin
-#samtools faidx /gpfs01/share/StickleAss/plotsr/Duin_clean.fasta \
-#--region-file /gpfs01/home/mbzlld/code_and_scripts/Regions_files/V5_regions_file.txt > /gpfs01/share/StickleAss/plotsr/Duin_cleaner.fasta
-##[faidx] Truncated sequence: chrUn:0-19879834
-##[faidx] Truncated sequence: chrX:0-17985176
-##[faidx] Truncated sequence: chrXIX:0-20580295
-##[faidx] Truncated sequence: chrXXI:0-17421465
-##[faidx] Truncated sequence: chrY:0-15859692
-
-## Scad
-#samtools faidx /gpfs01/share/StickleAss/plotsr/Scad_clean.fasta \
-#--region-file /gpfs01/home/mbzlld/code_and_scripts/Regions_files/V5_regions_file.txt > /gpfs01/share/StickleAss/plotsr/Scad_cleaner.fasta
-##[faidx] Truncated sequence: chrUn:0-19879834
-##[faidx] Truncated sequence: chrX:0-17985176
-##[faidx] Truncated sequence: chrY:0-15859692
-
-
-
-# # activate minimap
-# source $HOME/.bash_profile
-# conda activate minimap
-
-# # load modules you want to use
-# module load samtools-uoneasy/1.18-GCC-12.3.0
-
-# # specify the directory you want to write to
-# working_dir=/gpfs01/share/StickleAss/plotsr
-
-# # specify the locations of the assemblies you want to align
-# #duin_assembly=/gpfs01/home/mbzlld/data/Long_read_data/Duin_v1_ont_blobs.fa
-# #scad_assembly=/gpfs01/home/mbzlld/data/Long_read_data/Scad_v1_ont_blobs.fa
-# duin_assembly=Duin_cleaner.fasta
-# scad_assembly=Scad_cleaner.fasta
-
-# # look at the ~chromosome names in the two fasta files
-# grep ">" $working_dir/$duin_assembly
-# grep ">" $working_dir/$scad_assembly
-
-
-# # align the genomes
-# #asm=asm5
-# asm=asm10
-# #asm=asm20
-
-# # should also try -asm10 and -asm20 for up to 1% / 5% sequence divergence
-# minimap2 -ax $asm -t 16 --eqx $working_dir/$duin_assembly $working_dir/$scad_assembly -o $working_dir/duin_scad.sam
-# samtools sort $working_dir/duin_scad.sam -o $working_dir/duin_scad_$asm.bam
-# rm $working_dir/duin_scad.sam
-
-# # write the names of the assemblies to a file for use by plotsr
-# echo -e ""$working_dir"/"$duin_assembly"\tDuin
-# "$working_dir"/"$scad_assembly"\tScad" > /gpfs01/home/mbzlld/code_and_scripts/File_lists/plotsr_assemblies_list.txt
-
-# module unload samtools-uoneasy/1.12-GCC-9.3.0
-# conda deactivate
 
 
 # # create your syri environment (only have to do this once)
