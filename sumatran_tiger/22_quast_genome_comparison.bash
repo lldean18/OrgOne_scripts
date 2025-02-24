9920e81c9cf0e72ba4b9baa14b134ff62fd61ce0
Laura Dean <lldean18@gmail.com>
2025-02-24 12:00:46 +0000

    ran ok but 3 of the 4 assemblies got an OOM kill so increased the memory allocation from 30G to 80G and rerunning

--- sumatran_tiger/22_quast_genome_comparison.bash
+++ sumatran_tiger/22_quast_genome_comparison.bash
@@ -8,7 +8,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=64
-#SBATCH --mem=30g
+#SBATCH --mem=80g
 #SBATCH --time=6:00:00
 #SBATCH --output=/gpfs01/home/mbzlld/code_and_scripts/slurm_out_scripts/slurm-%x-%j.out
 
