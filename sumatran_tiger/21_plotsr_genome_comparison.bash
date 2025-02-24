7dc436247ff637c67227e3d424804dbd0ecfd8e1
Laura Dean <lldean18@gmail.com>
2025-02-24 13:33:00 +0000

    updating script after renaming the fasta headers in the reference to have a space between the header symbol and the text of the header. Hoping this will fix some issues with the script running

--- sumatran_tiger/21_plotsr_genome_comparison.bash
+++ sumatran_tiger/21_plotsr_genome_comparison.bash
@@ -22,43 +22,55 @@ asm1=/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/hifiasm_asm10/ONTasm.bp.p_ct
 source $HOME/.bash_profile
 module load samtools-uoneasy/1.18-GCC-12.3.0
 
+# first I fixed the fact that there were no spaces between > symbol and fasta headers with this line
+#sed -i 's/>/> /' GCA_018350195.2_chrs_only_uniq_names_nospaces.fasta
+# then proceeded
+
 #############################################
 #### assign our fragments to chromosomes ####
 #############################################
 
-## generate a regions file for filtering
-#echo "generating a regions file for filtering..."
-##conda create --name bioawk bioawk -y
-#conda activate bioawk
-#bioawk -c fastx '{print $name ":" 1 "-" length($seq)}' $reference > ${reference%.*}_regions_file.txt
-#conda deactivate
-#echo "Done"
+# generate a regions file for filtering
+echo "generating a regions file for filtering..."
+#conda create --name bioawk bioawk -y
+conda activate bioawk
+bioawk -c fastx '{print $name ":" 1 "-" length($seq)}' $reference > ${reference%.*}_regions_file.txt
+conda deactivate
+echo "Done"
 
-## align our assembly to the reference
-#echo "aligning our assembly to the reference..."
-#conda activate minimap2
-#minimap2 -x asm5 -t 16 $reference $asm1 > ${asm1%.*}_alignment.paf
-#conda deactivate
-#echo "Done"
+# align our assembly to the reference
+echo "aligning our assembly to the reference..."
+conda activate minimap2
+minimap2 -x asm5 -t 16 $reference $asm1 > ${asm1%.*}_alignment.paf
+conda deactivate
+echo "Done"
 
-## identify the best matching chromosomes for each of our fragments
-#echo "identifying the best matching chromosomes for each of our fragments..."
-#awk '{print $1, $6, $8-$7}' ${asm1%.*}_alignment.paf | sort -k1,1 -k3nr | awk '!seen[$1]++' > ${asm1%.*}_contig_assignments.txt
-#sed -i -r 's/[^ ]*$//' ${asm1%.*}_contig_assignments.txt # get rid of the contig lengths at the ends of the lines
-#sed -i -r 's/ /\t/' ${asm1%.*}_contig_assignments.txt # replace spaces with tabs
-#echo "Done"
+# identify the best matching chromosomes for each of our fragments
+echo "identifying the best matching chromosomes for each of our fragments..."
+awk '{print $1, $6, $8-$7}' ${asm1%.*}_alignment.paf | sort -k1,1 -k3nr | awk '!seen[$1]++' > ${asm1%.*}_contig_assignments.txt
+sed -i -r 's/[^ ]*$//' ${asm1%.*}_contig_assignments.txt # get rid of the contig lengths at the ends of the lines
+sed -i -r 's/ /\t/' ${asm1%.*}_contig_assignments.txt # replace spaces with tabs
+echo "Done"
 
-## rename contigs in our assembly based on their assignments
-#echo "renaming contigs in our assembly based on their assignments relative to the reference..."
-#conda activate seqkit
-#seqkit replace -p "^(.*)" -r "{kv}" --kv-file ${asm1%.*}_contig_assignments.txt $asm1 > ${asm1%.*}_ref_renamed_contigs.fasta
-#echo "Done"
+# rename contigs in our assembly based on their assignments
+echo "renaming contigs in our assembly based on their assignments relative to the reference..."
+conda activate seqkit
+seqkit replace -p "^(.*)" -r "{kv}" --kv-file ${asm1%.*}_contig_assignments.txt $asm1 > ${asm1%.*}_ref_renamed_contigs.fasta
+echo "Done"
 
-## filter so that only the longest contig for each match to the reference is retained
-#echo "retaining only the longest contig for each match to the reference..."
-#seqkit seq -j 4 -w 0 ${asm1%.*}_ref_renamed_contigs.fasta | seqkit rename | seqkit sort -l -r | sed 's/_[0-9][0-9]*//' | seqkit rmdup > ${asm1%.*}_ref_renamed_contigs_longest_sequences.fasta
-#conda deactivate
-#echo "Done"
+# filter so that only the longest contig for each match to the reference is retained
+echo "retaining only the longest contig for each match to the reference..."
+seqkit seq -j 4 -w 0 ${asm1%.*}_ref_renamed_contigs.fasta | seqkit rename | seqkit sort -l -r | sed 's/_[0-9][0-9]*//' | seqkit rmdup > ${asm1%.*}_ref_renamed_contigs_longest_sequences.fasta
+conda deactivate
+echo "Done"
+
+# Filter so that only contigs that are in our assembly are in a new version of the reference
+# because syri won't work with differing numbers of chromosomes in the same assembly
+echo "filtering the reference so that it only contains sequences that are in our assembly..."
+conda activate seqkit
+seqkit grep -f ${asm1%.*}_ref_renamed_contigs_longest_sequences.fasta $reference > ${reference%.*}_contigfilt.fasta
+conda deactivate
+echo "Done"
 
 ###########################################
 #### map our assembly to the reference ####
@@ -76,7 +88,7 @@ asm=asm10
 minimap2 \
 -ax $asm \
 -t 16 \
---eqx ${asm1%.*}_ref_renamed_contigs_longest_sequences.fasta $reference \
+--eqx ${asm1%.*}_ref_renamed_contigs_longest_sequences.fasta ${reference%.*}_contigfilt.fasta \
 -o $wkdir/tmp.sam
 samtools sort $wkdir/tmp.sam \
 -o $wkdir/$(basename ${asm1%.*})_$asm.bam
@@ -88,7 +100,7 @@ samtools index -bc $wkdir/$(basename ${asm1%.*})_$asm.bam
 
 # write the names of the assemblies to a file for use by plotsr
 echo -e ""${asm1%.*}_ref_renamed_contigs_longest_sequences.fasta"\tHifiasm10
-"$reference"\tReference" > $wkdir/$(basename ${asm1%.*})_plotsr_assemblies_list.txt
+"${reference%.*}_contigfilt.fasta"\tReference" > $wkdir/$(basename ${asm1%.*})_plotsr_assemblies_list.txt
 
 module unload samtools-uoneasy/1.18-GCC-12.3.0
 echo "Done"
@@ -105,7 +117,7 @@ conda activate syri
 # Run syri to find structural rearrangements between your assemblies
 syri \
 -c $wkdir/$(basename ${asm1%.*})_$asm.bam \
--r $reference \
+-r ${reference%.*}_contigfilt.fasta \
 -q ${asm1%.*}_ref_renamed_contigs_longest_sequences.fasta \
 -F B \
 --dir $wkdir \
