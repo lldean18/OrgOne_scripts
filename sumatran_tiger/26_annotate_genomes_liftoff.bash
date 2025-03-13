358f295c3cfa6a0b28532e2d2531cc229b1b35e2
Laura Dean <lldean18@gmail.com>
2025-03-13 11:44:43 +0000

    adding first shot at the script to liftoff cat genes to our assemblies. Tried for the Liger assembly first. Running the script now so currently it is untested

---
+++ sumatran_tiger/26_annotate_genomes_liftoff.bash
@@ -0,0 +1,70 @@
+#!/bin/bash
+# Laura Dean
+# 13/3/25
+# script written for running on the UoN HPC Ada
+
+#SBATCH --job-name=liftoff_annotate
+#SBATCH --partition=defq
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=32
+#SBATCH --mem=50g
+#SBATCH --time=48:00:00
+#SBATCH --output=/gpfs01/home/mbzlld/code_and_scripts/slurm_out_scripts/slurm-%x-%j.out
+
+
+# set variables
+reference_assembly=/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/domestic_cat_reference/AnAms1.0.genome.fa
+reference_gff=/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/domestic_cat_reference/AnAms1.0r1.0.2.gff
+target_assembly=/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/liger_reference/GCA_018350195.2_chrs_only_uniq_names.fasta
+
+
+# generate the chr matching file for the cat to our tiger genomes
+echo "AnAms1.0_A1,A1
+AnAms1.0_A2,A2
+AnAms1.0_A3,A3
+AnAms1.0_B1,B1
+AnAms1.0_B2,B2
+AnAms1.0_B3,B3
+AnAms1.0_B4,B4
+AnAms1.0_C1,C1
+AnAms1.0_C2,C2
+AnAms1.0_D1,D1
+AnAms1.0_D2,D2
+AnAms1.0_D3,D3
+AnAms1.0_D4,D4
+AnAms1.0_E1,E1
+AnAms1.0_E2,E2
+AnAms1.0_E3,E3
+AnAms1.0_F1,F1
+AnAms1.0_F2,F2
+AnAms1.0_X,X
+" > ${reference_assembly%.*}_chr_match_file.txt
+
+# generate the unplaced file for the unassigned scaffold in the cat genome
+echo "AnAms1.0_unplaced
+" > ${reference_assembly%.*}_unplaced.txt
+
+
+# load software
+source $HOME/.bash_profile
+#conda create --name liftoff -c bioconda liftoff
+conda activate liftoff
+
+
+# annotate assembly by lifting over genes from the closely related source assembly
+liftoff \
+	-g $reference_gff \
+	-o ${target_assembly%.*}_liftoff.gff \
+	-p 32 \
+	-chroms ${reference_assembly%.*}_chr_match_file.txt \
+	-unplaced ${reference_assembly%.*}_unplaced.txt \
+	$target_assembly $reference_assembly
+
+
+# unload software
+conda deactivate
+
+
+
+
