3900bdf85585fd56db2e90e7cba1df16f800534b
Laura Dean <lldean18@gmail.com>
2025-03-13 10:23:11 +0000

    shortening run time because of Ada shutdown

--- sumatran_tiger/24_annotate_genomes_augustus.bash
+++ sumatran_tiger/24_annotate_genomes_augustus.bash
@@ -9,7 +9,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=80g
-#SBATCH --time=120:00:00
+#SBATCH --time=50:00:00
 #SBATCH --output=/gpfs01/home/mbzlld/code_and_scripts/slurm_out_scripts/slurm-%x-%j.out
 
 
