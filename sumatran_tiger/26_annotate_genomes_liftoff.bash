e2498d6f6b5c97c3cefd541d3c7784310c976ca6
Laura Dean <lldean18@gmail.com>
2025-03-13 12:44:25 +0000

    updating the chromosome lists so they don't have a space at the end as this could be the cause of the error when the script runs

--- sumatran_tiger/26_annotate_genomes_liftoff.bash
+++ sumatran_tiger/26_annotate_genomes_liftoff.bash
@@ -38,12 +38,10 @@ AnAms1.0_E2,E2
 AnAms1.0_E3,E3
 AnAms1.0_F1,F1
 AnAms1.0_F2,F2
-AnAms1.0_X,X
-" > ${reference_assembly%.*}_chr_match_file.txt
+AnAms1.0_X,X" > ${reference_assembly%.*}_chr_match_file.txt
 
 # generate the unplaced file for the unassigned scaffold in the cat genome
-echo "AnAms1.0_unplaced
-" > ${reference_assembly%.*}_unplaced.txt
+echo "AnAms1.0_unplaced" > ${reference_assembly%.*}_unplaced.txt
 
 
 # load software
