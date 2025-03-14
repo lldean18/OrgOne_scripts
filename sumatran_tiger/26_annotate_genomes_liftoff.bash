7a84918ad61e6d63c2040a7c000236f2ba20c1be
Laura Dean <lldean18@gmail.com>
2025-03-14 10:14:50 +0000

    since the liftoff annotation still does not seem to be working for the liger reference I'm trying it with the hifiasm10 assembly, with no prior info about which contig maps to which, just to see if its anything to do with the liger assembly

--- sumatran_tiger/26_annotate_genomes_liftoff.bash
+++ sumatran_tiger/26_annotate_genomes_liftoff.bash
@@ -9,7 +9,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=32
 #SBATCH --mem=50g
-#SBATCH --time=48:00:00
+#SBATCH --time=35:00:00
 #SBATCH --output=/gpfs01/home/mbzlld/code_and_scripts/slurm_out_scripts/slurm-%x-%j.out
 
 
@@ -17,31 +17,32 @@
 reference_assembly=/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/domestic_cat_reference/AnAms1.0.genome.fa
 reference_gff=/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/domestic_cat_reference/AnAms1.0r1.0.2.gff
 target_assembly=/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/liger_reference/GCA_018350195.2_chrs_only_uniq_names.fasta
+target_assembly=/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/hifiasm_asm10/ONTasm.bp.p_ctg_100kb.fasta
 
 
-# generate the chr matching file for the cat to our tiger genomes
-echo "AnAms1.0_A1,A1
-AnAms1.0_A2,A2
-AnAms1.0_A3,A3
-AnAms1.0_B1,B1
-AnAms1.0_B2,B2
-AnAms1.0_B3,B3
-AnAms1.0_B4,B4
-AnAms1.0_C1,C1
-AnAms1.0_C2,C2
-AnAms1.0_D1,D1
-AnAms1.0_D2,D2
-AnAms1.0_D3,D3
-AnAms1.0_D4,D4
-AnAms1.0_E1,E1
-AnAms1.0_E2,E2
-AnAms1.0_E3,E3
-AnAms1.0_F2,F2
-AnAms1.0_X,X" > ${reference_assembly%.*}_chr_match_file.txt
-
-# generate the unplaced file for the unassigned scaffold in the cat genome
-echo "AnAms1.0_F1
-AnAms1.0_unplaced" > ${reference_assembly%.*}_unplaced.txt
+## generate the chr matching file for the cat to our tiger genomes
+#echo "AnAms1.0_A1,A1
+#AnAms1.0_A2,A2
+#AnAms1.0_A3,A3
+#AnAms1.0_B1,B1
+#AnAms1.0_B2,B2
+#AnAms1.0_B3,B3
+#AnAms1.0_B4,B4
+#AnAms1.0_C1,C1
+#AnAms1.0_C2,C2
+#AnAms1.0_D1,D1
+#AnAms1.0_D2,D2
+#AnAms1.0_D3,D3
+#AnAms1.0_D4,D4
+#AnAms1.0_E1,E1
+#AnAms1.0_E2,E2
+#AnAms1.0_E3,E3
+#AnAms1.0_F2,F2
+#AnAms1.0_X,X" > ${reference_assembly%.*}_chr_match_file.txt
+#
+## generate the unplaced file for the unassigned scaffold in the cat genome
+#echo "AnAms1.0_F1
+#AnAms1.0_unplaced" > ${reference_assembly%.*}_unplaced.txt
 
 
 # load software
