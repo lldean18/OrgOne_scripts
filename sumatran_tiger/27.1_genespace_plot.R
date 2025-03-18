e5a977d4b78292a63a52a1faf8d0f7394b1adbd2
Laura Dean <lldean18@gmail.com>
2025-03-18 11:51:38 +0000

    full tutorial with my paths etc added, ready for testing

--- sumatran_tiger/27.1_genespace_plot.R
+++ sumatran_tiger/27.1_genespace_plot.R
@@ -1,12 +1,61 @@
-
+#########################################################################
 # Load libraries
 library(GENESPACE)
 
+
+#########################################################################
 # set paths for genespace to use 
-genomeRepo <- "~/path/to/store/rawGenomes"
-wd <- "~/path/to/genespace/workingDirectory"
+genomeRepo <- "/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/genespace/data"
+wd <- "/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/genespace"
 path2mcscanx <- "~/path/to/MCScanX/"
 
+
+
+#########################################################################
+# download the practice data
+urls <- c(
+  human ="000/001/405/GCF_000001405.40_GRCh38.p14/GCF_000001405.40_GRCh38.p14_",
+  mouse = "000/001/635/GCF_000001635.27_GRCm39/GCF_000001635.27_GRCm39_",
+  platypus = "004/115/215/GCF_004115215.2_mOrnAna1.pri.v4/GCF_004115215.2_mOrnAna1.pri.v4_",
+  chicken = "016/699/485/GCF_016699485.2_bGalGal1.mat.broiler.GRCg7b/GCF_016699485.2_bGalGal1.mat.broiler.GRCg7b_",
+  sandLizard = "009/819/535/GCF_009819535.1_rLacAgi1.pri/GCF_009819535.1_rLacAgi1.pri_")
+
+genomes2run <- names(urls)
+urls <- file.path("https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF", urls)
+translatedCDS <- sprintf("%stranslated_cds.faa.gz", urls)
+geneGff <- sprintf("%sgenomic.gff.gz", urls)
+
+names(translatedCDS) <- genomes2run
+names(geneGff) <- genomes2run
+writeDirs <- file.path(genomeRepo, genomes2run)
+names(writeDirs) <- genomes2run
+for(i in genomes2run){
+  print(i)
+  if(!dir.exists(writeDirs[i]))
+    dir.create(writeDirs[i])
+  download.file(
+    url = geneGff[i], 
+    destfile = file.path(writeDirs[i], basename(geneGff[i])))
+  download.file(
+    url = translatedCDS[i], 
+    destfile = file.path(writeDirs[i], basename(translatedCDS[i])))
+}
+
+
+#########################################################################
+# use genespace to parse the annotations
+genomes2run <- c("human", "mouse", "platypus", "chicken", "sandLizard")
+parsedPaths <- parse_annotations(
+  rawGenomeRepo = genomeRepo,
+  genomeDirs = genomes2run,
+  genomeIDs = genomes2run,
+  presets = "ncbi",
+  genespaceWd = wd)
+
+
+
+
+#########################################################################
 # initialise the genespace run and set the run parameters
 gpar <- init_genespace(
   wd = wd,
@@ -15,6 +64,10 @@ gpar <- init_genespace(
 # run genespace
 out <- run_genespace(gpar, overwrite = T)
 
+
+
+
+#########################################################################
 # generate riparian plot
 ripd <- plot_riparian(
   gsParam = out,
@@ -26,4 +79,3 @@ hits <- read_allBlast(
   filepath = file.path(out$paths$syntenicHits, 
                        "mouse_vs_human.allBlast.txt.gz"))
 ggdotplot(hits = hits, type = "all", verbose = FALSE)
-
