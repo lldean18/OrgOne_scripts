78356a0f980abde5fa259e1f0e224c0a71bd44d4
Laura Dean <lldean18@gmail.com>
2025-03-20 16:34:29 +0000

    code used to prep the domestic cat assembly files for input to genespace

---
+++ sumatran_tiger/27.0_prep_assemblies_for_genespace.bash
@@ -0,0 +1,33 @@
+#!/bin/bash
+# Laura Dean
+# file prep for input to genespace to make a nice synteny plot
+
+
+source $HOME/.bash_profile
+
+# set variables
+assembly=/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/domestic_cat_reference/AnAms1.0.genome.fa
+annotation=/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/domestic_cat_reference/AnAms1.0r1.0.2.gff
+
+
+# load software
+conda activate gffread
+
+# convert the gff file to bed format for input to genespace
+# this command seems to retain only the genes anyway so no need for further filtering
+gffread \
+$annotation \
+--bed \
+-o ${assembly%.*}_genes.bed
+
+
+# generate protein fasta file as this is also required as input to genespace
+# -S flag uses * instead of . for stop codon translation
+gffread \
+$annotation \
+-S \
+-g $assembly \
+-y ${assembly%.*}_proteins.fasta
+
+# unload software
+conda deactivate
