ab2c0ceef4ebd001b7dc880322eb64665c6d2c8a
Laura Dean <lldean18@gmail.com>
2025-03-25 11:22:37 +0000

    fixing a typo and updating to run on a new assembly

--- sumatran_tiger/16_get_n50.bash
+++ sumatran_tiger/16_get_n50.bash
@@ -1,10 +1,11 @@
 
 # create conda environment
 #conda create --name n50 -c bioconda n50
-conda acitvate n50
+conda activate n50
 
 # Set the assembly file you want to get stats for
 assembly=/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/raft_hifiasm_asm9/finalasm.bp.p_ctg.fasta
+assembly=/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/hifiasm_asm11/ONTasm.bp.p_ctg.fasta
 
 # print the n50 to the screen
 n50 $assembly
