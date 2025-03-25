a4b4e870015888ce1911befe8fec0123ce998c31
Laura Dean <lldean18@gmail.com>
2025-03-25 11:14:06 +0000

    adding script to generate an assembly for comparison with the same reads and version of hifiasm but without the --ont flag

---
+++ sumatran_tiger/07.11_tig_hifiasm0.24.0_no--ONT_assembly.bash
@@ -0,0 +1,57 @@
+#!/bin/bash
+# Laura Dean
+# 21/3/25
+# for running on Ada
+
+#SBATCH --partition=hmemq
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=96
+#SBATCH --mem=1495g
+#SBATCH --time=24:00:00
+#SBATCH --job-name=tig_hifiasm0.24.0
+#SBATCH --output=/gpfs01/home/mbzlld/code_and_scripts/slurm_out_scripts/slurm-%x-%j.out
+
+source $HOME/.bash_profile
+
+####### PREPARE ENVIRONMENT #######
+# create conda environment
+#conda create --name hifiasm_0.24.0 hifiasm -y
+conda activate hifiasm_0.24.0
+
+
+# set environment variables
+species=sumatran_tiger # set the species
+wkdir=/gpfs01/home/mbzlld/data/OrgOne/$species # set the working directory
+# set the attempt number for naming the output directory of each try
+# then set the reads file that was used in that attempt
+#attempt=9
+#reads=$wkdir/basecalls/all_simplex_simplex.fastq.gz # running the file with the best previous assembly in the newest version of hifiasm
+#attempt=10
+#reads=$wkdir/basecalls/simplex_simplex_and_duplex.fastq.gz # running with the simplex runs merged with the duplex extracted from duplex runs
+attempt=11
+reads=$wkdir/basecalls/simplex_simplex_and_duplex.fastq.gz # running with the simplex runs merged with the duplex extracted from duplex runs
+
+# print a line to the slurm output that says exactly what was done on this run
+echo "This is hifiasm version 0.24.0 running on the file $reads WITHOUT the new --ONT flag and saving the output to the directory $wkdir/hifiasm_asm$attempt"
+
+# make directory for the assembly & move to it
+mkdir -p $wkdir/hifiasm_asm$attempt
+cd $wkdir/hifiasm_asm$attempt
+
+# run hifiasm on the simplex reads withOUT the new --ont flag to generate the assembly
+hifiasm \
+-t 96 \
+-o ONTasm \
+$reads
+
+
+#--ont \
+
+
+# convert the final assembly to fasta format
+awk '/^S/{print ">"$2;print $3}' ONTasm.bp.p_ctg.gfa > ONTasm.bp.p_ctg.fasta
+
+# deactivate conda
+conda deactivate
+
