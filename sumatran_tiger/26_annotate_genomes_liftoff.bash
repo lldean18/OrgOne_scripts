086e1d9a09f8cb4340fa9c794258f80021974423
Laura Dean <lldean18@gmail.com>
2025-03-31 10:24:00 +0100

    updating to run on hifiasm12 done with version 0.25.0 of hifiasm

--- sumatran_tiger/26_annotate_genomes_liftoff.bash
+++ sumatran_tiger/26_annotate_genomes_liftoff.bash
@@ -21,7 +21,9 @@ reference_gff=/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/domestic_cat_refere
 #target_assembly=/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/hifiasm_asm10/ONTasm.bp.p_ctg_100kb.fasta # Worked successfully
 #target_assembly=/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/raft_hifiasm_asm9/finalasm.bp.p_ctg.fasta # worked successfully
 #target_assembly=/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/sumatran_tiger_flye_asm4/assembly.fasta
-target_assembly=/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/hifiasm_asm11/ONTasm.bp.p_ctg_100kb.fasta
+#target_assembly=/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/hifiasm_asm11/ONTasm.bp.p_ctg_100kb.fasta
+target_assembly=/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/hifiasm_asm12/ONTasm.bp.p_ctg_100kb.fasta
+
 
 # load software
 #conda create --name liftoff -c bioconda liftoff
