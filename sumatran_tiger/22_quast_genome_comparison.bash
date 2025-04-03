b0d7dde3b995ea0643348e51c20b344ca94aafec
Laura Dean <lldean18@gmail.com>
2025-04-03 17:31:42 +0100

    updating quast script to run on the newest assembly files and use the domestic cat genome for the reference

--- sumatran_tiger/22_quast_genome_comparison.bash
+++ sumatran_tiger/22_quast_genome_comparison.bash
@@ -1,13 +1,14 @@
 #!/bin/bash
 # Laura Dean
 # 21/2/25
+# 3/4/25
 # script written for running on the UoN HPC Ada
 
 #SBATCH --job-name=quast
 #SBATCH --partition=defq
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=96
+#SBATCH --cpus-per-task=32
 #SBATCH --mem=360g
 #SBATCH --time=48:00:00
 #SBATCH --output=/gpfs01/home/mbzlld/code_and_scripts/slurm_out_scripts/slurm-%x-%j.out
@@ -26,11 +27,11 @@ conda activate quast
 
 # compare genomes
 python /gpfs01/home/mbzlld/software_bin/miniconda3/envs/quast/bin/quast \
-	--threads 96 \
+	--threads 32 \
 	--eukaryote \
 	-r $reference \
 	-g $annotation \
-	-o $wkdir/quast $wkdir/hifiasm_asm10/ONTasm.bp.p_ctg_100kb.fasta $wkdir/hifiasm_asm9/ONTasm.bp.p_ctg_100kb.fasta $wkdir/hifiasm_asm8/ONTasm.bp.p_ctg.fasta $wkdir/hifiasm_asm1/ONTasm.bp.p_ctg.fasta
+	-o $wkdir/quast2 $wkdir/liger_reference/GCA_018350195.2_scaff_only_names_split_contigs.fasta $wkdir/hifiasm_asm10/ONTasm.bp.p_ctg_100kb.fasta $wkdir/raft_hifiasm_asm10/finalasm.bp.p_ctg_100kb.fasta $wkdir/sumatran_tiger_flye_asm4/assembly.fasta
 
 
 conda deactivate
