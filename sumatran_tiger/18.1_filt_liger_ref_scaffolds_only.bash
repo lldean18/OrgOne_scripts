c5560b4505df6bfa22c1bdd35f4f424151bb3392
Laura Dean <lldean18@gmail.com>
2025-04-03 09:58:41 +0100

    updating the filtering file for the liger reference to split it at the Ns

--- sumatran_tiger/18.1_filt_liger_ref_scaffolds_only.bash
+++ sumatran_tiger/18.1_filt_liger_ref_scaffolds_only.bash
@@ -7,7 +7,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=1g
+#SBATCH --mem=10g
 #SBATCH --time=3:00:00
 #SBATCH --job-name=filt_lig_ref
 #SBATCH --output=/gpfs01/home/mbzlld/code_and_scripts/slurm_out_scripts/slurm-%x-%j.out
@@ -20,13 +20,30 @@ headers=/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/liger_reference/GCA_01835
 output=/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/liger_reference/GCA_018350195.2_scaff_only.fasta.gz
 
 source $HOME/.bash_profile
-conda activate seqtk
+#conda activate seqtk
+#
+#seqtk subseq $fasta $headers | gzip > $output
+#
+#conda deactivate
+#
+## rename the fasta headers to something sensible for viewing
+## (so that the names of the chrs are visible in the dotplot)
+#zcat $output | sed "s/^.*scaffold_/>/;s/, whole genome shotgun sequence.//" | gzip > ${output%.*.*}_names.fasta.gz
 
-seqtk subseq $fasta $headers | gzip > $output
+
+conda activate seqkit
+
+# split the scaffolds into contigs (splitting at every occurence of N)
+zcat ${output%.*.*}_names.fasta.gz |
+	seqkit fx2tab |
+	cut -f 2 |
+	sed -r 's/n+/\n/gi' |
+	cat -n |
+	seqkit tab2fx |
+	seqkit replace -p "(.+)" -r "Contig{nr}" |
+	gzip > ${output%.*.*}_names_split_contigs.fasta.gz
+
 
 conda deactivate
 
-# rename the fasta headers to something sensible for viewing
-# (so that the names of the chrs are visible in the dotplot)
-zcat $output | sed "s/^.*scaffold_/>/;s/, whole genome shotgun sequence.//" | gzip > ${output%.*.*}_names.fasta.gz
 
