e3334bdfd20df56a056d04aede2b00f791ec46af
Laura Dean <lldean18@gmail.com>
2025-04-05 07:10:43 +0100

    adding line to source bash profile so conda works

--- sumatran_tiger/28_scaffold_with_HiC.bash
+++ sumatran_tiger/28_scaffold_with_HiC.bash
@@ -12,6 +12,7 @@
 #SBATCH --time=60:00:00
 #SBATCH --output=/gpfs01/home/mbzlld/code_and_scripts/slurm_out_scripts/slurm-%x-%j.out
 
+source $HOME/.bash_profile
 
 ###########################################################
 # set working directory
