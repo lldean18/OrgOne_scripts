6c91e1d195260fe8e89d6a00132038d55cbb3399
Laura Dean <lldean18@gmail.com>
2025-04-07 11:49:21 +0100

    updating to fix another typo in the syntax of picard markduplicates command

--- sumatran_tiger/28_scaffold_with_HiC.bash
+++ sumatran_tiger/28_scaffold_with_HiC.bash
@@ -55,7 +55,7 @@ picard MarkDuplicates \
 	-I $(basename ${assembly%.*})_hic_mapped_sorted.bam \
 	-O $(basename ${assembly%.*})_hic_mapped_sorted_dupmk.bam \
 	-M $(basename ${assembly%.*})_picard_dup_metrics.txt \
-	--CREATE_INDEX=true
+	--CREATE_INDEX true
 
 
 conda deactivate
