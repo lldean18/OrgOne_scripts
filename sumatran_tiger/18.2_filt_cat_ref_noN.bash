7641a8a4b94fa5ed1950f0dbdafdb175d1d2fd81
Laura Dean <lldean18@gmail.com>
2025-04-09 09:00:18 +0100

    testing whether the awk block worked by running the old block to compare output. This is since the result of the awk block didnt split anything but grep showed Ns

---
+++ sumatran_tiger/18.2_filt_cat_ref_noN.bash
@@ -0,0 +1,49 @@
+#!/bin/bash
+# Laura Dean
+# 8/4/25
+# for running on the UoN HPC Ada
+
+#SBATCH --partition=defq
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=10g
+#SBATCH --time=3:00:00
+#SBATCH --job-name=filt_cat_ref
+#SBATCH --output=/gpfs01/home/mbzlld/code_and_scripts/slurm_out_scripts/slurm-%x-%j.out
+
+
+assembly=/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/domestic_cat_reference/AnAms1.0.genome.fa
+
+source $HOME/.bash_profile
+conda activate seqkit
+
+## split the scaffolds into contigs (splitting at every occurence of N)
+#seqkit fx2tab -n -s $assembly |
+#awk -F '\t' '
+#{
+#    header = $1;
+#    seq = $2;
+#    split(seq, fragments, /[nN]+/);
+#    for (i = 1; i <= length(fragments); i++) {
+#        if (length(fragments[i]) > 0) {
+#            print header "_" i "\t" fragments[i];
+#        }
+#    }
+#}' | \
+#seqkit tab2fx > ${assembly%.*}_split_contigs.fa
+
+
+
+cat $assembly |
+	seqkit fx2tab |
+	cut -f 2 |
+	sed -r 's/n+/\n/gi' |
+	cat -n |
+	seqkit tab2fx |
+	seqkit replace -p "(.+)" -r "Contig{nr}" > ${assembly%.*}_split_contigs_test.fa
+
+
+conda deactivate
+
+
