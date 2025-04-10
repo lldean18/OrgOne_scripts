a52e386a7cae719619bcfc4940be53beb3f9ac14
Laura Dean <lldean18@gmail.com>
2025-04-10 14:05:13 +0100

    reverting to the command that worked but didn't preserve the old names of the scaffolds since the new awk script is not working and I don't really need it to do that at this point

--- sumatran_tiger/18.2_filt_cat_ref_noN.bash
+++ sumatran_tiger/18.2_filt_cat_ref_noN.bash
@@ -18,45 +18,14 @@ assembly=/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/domestic_cat_reference/A
 source $HOME/.bash_profile
 conda activate seqkit
 
-## split the scaffolds into contigs (splitting at every occurence of N)
-#seqkit fx2tab -n -s $assembly |
-#awk -F '\t' '
-#{
-#    header = $1;
-#    seq = $2;
-#    split(seq, fragments, /[nN]+/);
-#    for (i = 1; i <= length(fragments); i++) {
-#        if (length(fragments[i]) > 0) {
-#            print header "_" i "\t" fragments[i];
-#        }
-#    }
-#}' | \
-#seqkit tab2fx > ${assembly%.*}_split_contigs.fa
-
-seqkit fx2tab -n -s $assembly |
-awk -F '\t' '
-{
-    header = $1;
-    seq = $2;
-    n = split(seq, fragments, /[nN]+/);
-    for (i = 1; i <= n; i++) {
-        if (length(fragments[i]) > 0) {
-            print header "_" i "\t" fragments[i];
-        }
-    }
-}' |
-seqkit tab2fx > ${assembly%.*}_split_contigs_test2.fa
-
-
-
-
+# split the scaffolds into contigs (splitting at every occurence of N)
 cat $assembly |
 	seqkit fx2tab |
 	cut -f 2 |
 	sed -r 's/n+/\n/gi' |
 	cat -n |
 	seqkit tab2fx |
-	seqkit replace -p "(.+)" -r "Contig{nr}" > ${assembly%.*}_split_contigs_test.fa
+	seqkit replace -p "(.+)" -r "Contig{nr}" > ${assembly%.*}_split_contigs.fa
 
 
 conda deactivate
