286a20ee32c16251caff3607f4e3712a8b30c938
Laura Dean <lldean18@gmail.com>
2025-04-11 09:42:43 +0100

    modifying to annotate the domestic cat assembly with split contigs

--- sumatran_tiger/26_annotate_genomes_liftoff.bash
+++ sumatran_tiger/26_annotate_genomes_liftoff.bash
@@ -25,7 +25,9 @@ reference_gff=/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/domestic_cat_refere
 #target_assembly=/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/hifiasm_asm11/ONTasm.bp.p_ctg_100kb.fasta # worked
 #target_assembly=/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/hifiasm_asm12/ONTasm.bp.p_ctg_100kb.fasta # worked
 #target_assembly=/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/raft_hifiasm_asm10/finalasm.bp.p_ctg_100kb.fasta # worked
-target_assembly=/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/liger_reference/GCA_018350195.2_scaff_only_names_split_contigs.fasta
+#target_assembly=/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/liger_reference/GCA_018350195.2_scaff_only_names_split_contigs.fasta
+target_assembly=/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/domestic_cat_reference/AnAms1.0.genome_split_contigs.fa
+
 
 # load software
 #conda create --name liftoff -c bioconda liftoff
