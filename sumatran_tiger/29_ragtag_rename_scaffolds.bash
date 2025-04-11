70b8a010b4730a81af4bc5cad6fd1421637d617e
Laura Dean <lldean18@gmail.com>
2025-04-11 15:31:50 +0100

    adding script to rename scaffolds to match the domestic cat assembly using ragtag

---
+++ sumatran_tiger/29_ragtag_rename_scaffolds.bash
@@ -0,0 +1,35 @@
+#!/bin/bash
+# Laura Dean
+# 11/4/25
+# script written for running on the UoN HPC Ada
+
+#SBATCH --job-name=ragtag_rename
+#SBATCH --partition=defq
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=32
+#SBATCH --mem=50g
+#SBATCH --time=6:00:00
+#SBATCH --output=/gpfs01/home/mbzlld/code_and_scripts/slurm_out_scripts/slurm-%x-%j.out
+
+
+# set variables
+reference=/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/domestic_cat_reference/AnAms1.0.genome.fa
+assembly=/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/HiC/ONTasm.bp.p_ctg_100kb_yahs_scaffolds_final.fa
+
+
+source $HOME/.bash_profile
+# create and activate conda env
+#conda create --name ragtag ragtag
+conda activate ragtag
+
+
+# rename scaffolds based on domestic cat reference genome
+ragtag.py scaffold -t 32 -o ${assembly%.*}_ragtag $reference $assembly
+
+
+
+# deactivate software
+conda deactivate
+
+
