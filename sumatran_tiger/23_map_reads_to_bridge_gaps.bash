11ba72d19e658510c19e70abe887d358e33f8a81
Laura Dean <lldean18@gmail.com>
2025-04-25 11:58:43 +0100

    Reference and reads were input to minimap2 in the wrong order switched them and this version shoud run without segfault or errors now

--- sumatran_tiger/23_map_reads_to_bridge_gaps.bash
+++ sumatran_tiger/23_map_reads_to_bridge_gaps.bash
@@ -1,15 +1,15 @@
 #!/bin/bash
 # Laura Dean
-# 7/3/25
+# 25/4/25
 # script written for running on the UoN HPC Ada
 
-#SBATCH --job-name=id_gap_spanning_reads
+#SBATCH --job-name=map_reads_to_ref
 #SBATCH --partition=defq
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=32
-#SBATCH --mem=361g
-#SBATCH --time=36:00:00
+#SBATCH --cpus-per-task=34
+#SBATCH --mem=200g
+#SBATCH --time=48:00:00
 #SBATCH --output=/gpfs01/home/mbzlld/code_and_scripts/slurm_out_scripts/slurm-%x-%j.out
 
 
@@ -19,31 +19,32 @@ conda activate minimap2
 
 
 #reads=/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/basecalls/simplex_simplex_and_duplex.fastq.gz
-reads=/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/basecalls/all_simplex_simplex.fastq.gz
+#reads=/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/basecalls/all_simplex_simplex.fastq.gz
+reads=/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/basecalls/all_duplex.fastq.gz
 assembly=/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/hifiasm_asm10/ONTasm.bp.p_ctg_100kb.fasta
 
+suffix=duplex
 
 # map the raw reads back to our assembly
 minimap2 \
 	-a \
 	-x map-ont \
-	-K 100 \
-	-t 32 \
 	--split-prefix temp_prefix \
-	-o ${assembly%.*}.sam \
-	$reads $assembly
+	-t 32 \
+	-o ${assembly%.*}_$suffix.sam \
+	$assembly $reads
 
 
-## sort and index the sam file and convert to bam format
-#samtools sort \
-#	--threads 63 \
-#	--write-index \
-#	--output-fmt BAM \
-#	-o ${assembly%.*}.bam ${assembly%.*}.sam
+# sort and index the sam file and convert to bam format
+samtools sort \
+	--threads 32 \
+	--write-index \
+	--output-fmt BAM \
+	-o ${assembly%.*}_$suffix.bam ${assembly%.*}_$suffix.sam
 
 
 # remove the intermediate sam file
-#rm ${assembly%.*}.sam
+rm ${assembly%.*}_$suffix.sam
 
 
 # deactivate software
