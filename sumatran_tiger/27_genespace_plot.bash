33af6f8aed49a31d809efeb485aed6f5a4e8a436
Laura Dean <lldean18@gmail.com>
2025-04-25 13:43:19 +0100

    modifying to run in hifiasm9 as well

--- sumatran_tiger/27_genespace_plot.bash
+++ sumatran_tiger/27_genespace_plot.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Laura Dean
-# 14/3/25
+# 25/4/25
 # script written for running on the UoN HPC Ada
 
 
@@ -28,6 +28,7 @@ wkdir=~/data/OrgOne/sumatran_tiger/genespace_ours5
 wkdir=~/data/OrgOne/sumatran_tiger/genespace_ours6
 wkdir=~/data/OrgOne/sumatran_tiger/genespace_ours7
 wkdir=~/data/OrgOne/sumatran_tiger/genespace_ours8
+wkdir=~/data/OrgOne/sumatran_tiger/genespace_ours9
 
 
 
@@ -50,6 +51,8 @@ cut -f1-4 ~/data/OrgOne/sumatran_tiger/sumatran_tiger_flye_asm4/assembly_liftoff
 cut -f1-4 ~/data/OrgOne/sumatran_tiger/hifiasm_asm11/ONTasm.bp.p_ctg_100kb_liftoff_genes.bed > $wkdir/bed/hifiasm11.bed
 cut -f1-4 ~/data/OrgOne/sumatran_tiger/domestic_cat_reference/AnAms1.0.genome_split_contigs_liftoff_genes.bed > $wkdir/bed/DomesticCatContig.bed
 cut -f1-4 ~/data/OrgOne/sumatran_tiger/HiC/ONTasm.bp.p_ctg_100kb_yahs_scaffolds_final_liftoff_genes.bed > $wkdir/bed/hifiasm10HiC.bed
+cut -f1-4 ~/data/OrgOne/sumatran_tiger/hifiasm_asm9/ONTasm.bp.p_ctg_100kb_liftoff_genes.bed > $wkdir/bed/hifiasm9.bed
+
 
 # copy my protein fasta files
 # currently erroring because there are . characters in the fasta sequence and these aren't allowed by diamond
@@ -63,6 +66,7 @@ cp ~/data/OrgOne/sumatran_tiger/sumatran_tiger_flye_asm4/assembly_proteins.fasta
 cp ~/data/OrgOne/sumatran_tiger/hifiasm_asm11/ONTasm.bp.p_ctg_100kb_proteins.fasta $wkdir/peptide/hifiasm11.fa
 cp ~/data/OrgOne/sumatran_tiger/domestic_cat_reference/AnAms1.0.genome_split_contigs_proteins.fasta $wkdir/peptide/DomesticCatContig.fa
 cp ~/data/OrgOne/sumatran_tiger/HiC/ONTasm.bp.p_ctg_100kb_yahs_scaffolds_final_proteins.fasta $wkdir/peptide/hifiasm10HiC.fa
+cp ~/data/OrgOne/sumatran_tiger/hifiasm_asm9/ONTasm.bp.p_ctg_100kb_proteins.fasta $wkdir/peptide/hifiasm9.fa
 
 
 
