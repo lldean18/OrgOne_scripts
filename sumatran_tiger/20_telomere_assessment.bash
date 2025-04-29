209216163397451f9a5106c01068417cbf1404d5
Laura Dean <lldean18@gmail.com>
2025-04-29 15:17:58 +0100

    updating script to run on domestic cat genome

--- sumatran_tiger/20_telomere_assessment.bash
+++ sumatran_tiger/20_telomere_assessment.bash
@@ -34,8 +34,17 @@ conda activate quartet
 ##genome=GCA_018350195.2_chrs_only_uniq_names_nospaces.fasta
 #genome=GCA_018350195.2_scaff_only_names.fasta.gz
 
-wkdir=/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/HiC/ONTasm.bp.p_ctg_100kb_yahs_scaffolds_final_ragtag
-genome=ragtag.scaffold.fasta
+#wkdir=/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/HiC/ONTasm.bp.p_ctg_100kb_yahs_scaffolds_final_ragtag
+#genome=ragtag.scaffold.fasta
+
+#wkdir=/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/raft_hifiasm_asm10
+#genome=finalasm.bp.p_ctg_100kb.fasta
+
+#wkdir=/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/sumatran_tiger_flye_asm4
+#genome=assembly_100kb.fasta
+
+wkdir=/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/domestic_cat_reference
+genome=AnAms1.0.genome_split_contigs_100kb.fasta
 
 # move to working directory
 cd $wkdir
