e1f43111dd4ddb77bbbfc59ea16384ed572b96c2
Laura Dean <lldean18@gmail.com>
2025-04-30 11:55:31 +0100

    updating hic scaffolding script to run for the hifiasm9 simplex only assembly

--- sumatran_tiger/28_scaffold_with_HiC.bash
+++ sumatran_tiger/28_scaffold_with_HiC.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Laura Dean
-# 3/4/25
+# 30/4/25
 # for running on the UoN HPC Ada
 
 #SBATCH --job-name=hic_scaffold
@@ -20,15 +20,19 @@ wkdir=/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger
 
 # Make a dir for the hic data
 #mkdir $wkdir/HiC
+mkdir $wkdir/HiC2
+
+#cd $wkdir/HiC
+cd $wkdir/HiC2
 
 # download HiC data
-cd $wkdir/HiC
 #wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR861/005/SRR8616865/SRR8616865_1.fastq.gz
 #wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/SRR861/005/SRR8616865/SRR8616865_2.fastq.gz
 
 
 # set variables
-assembly=$wkdir/hifiasm_asm10/ONTasm.bp.p_ctg_100kb.fasta
+#assembly=$wkdir/hifiasm_asm10/ONTasm.bp.p_ctg_100kb.fasta
+assembly=$wkdir/hifiasm_asm9/ONTasm.bp.p_ctg_100kb.fasta
 
 hic1=~/data/OrgOne/sumatran_tiger/HiC/SRR8616865_1.fastq.gz
 hic2=~/data/OrgOne/sumatran_tiger/HiC/SRR8616865_2.fastq.gz
@@ -40,14 +44,14 @@ hic2=~/data/OrgOne/sumatran_tiger/HiC/SRR8616865_2.fastq.gz
 #conda install picard
 conda activate bwa
 
-## index the assembly fasta file
-#bwa index $assembly
+# index the assembly fasta file
+bwa index $assembly
 
-## align the hic reads to the assembly and convert to BAM
-#bwa mem -t 32 -S -P -5 $assembly $hic1 $hic2 |
-#	samtools view -@ 32 -b -q 30 - > $(basename ${assembly%.*})_hic_mapped.bam
+# align the hic reads to the assembly and convert to BAM
+bwa mem -t 32 -S -P -5 $assembly $hic1 $hic2 |
+	samtools view -@ 32 -b -q 30 - > $(basename ${assembly%.*})_hic_mapped.bam
 
-## sort the bam by read name
+# sort the bam by read name
 #samtools sort -@ 32 -n $(basename ${assembly%.*})_hic_mapped.bam |
 ## mark pcr duplicates
 #samtools fixmate -m -@ 32 - - |
@@ -100,20 +104,20 @@ conda deactivate
 #conda create --name yahs yahs -y
 conda activate yahs
 
-## index the assembly with samtools
-#samtools faidx $assembly
+# index the assembly with samtools
+samtools faidx $assembly
 
-## scaffold with hic data
-#yahs \
-#	-o $wkdir/HiC/$(basename ${assembly%.*})_yahs \
-#	$assembly \
-#	$(basename ${assembly%.*})_hic_mapped.bam
+# scaffold with hic data
+yahs \
+	-o $wkdir/HiC/$(basename ${assembly%.*})_yahs \
+	$assembly \
+	$(basename ${assembly%.*})_hic_mapped.bam
 
 ###########################################################
-## prepare the files for visualisation in juicebox
-#(juicer pre $(basename ${assembly%.*})_yahs.bin $(basename ${assembly%.*})_yahs_scaffolds_final.agp $assembly.fai |
-#       	sort -k2,2d -k6,6d -T ./ --parallel=8 -S32G |
-#       	awk 'NF' > alignments_sorted.txt.part) && (mv alignments_sorted.txt.part alignments_sorted.txt)
+# prepare the files for visualisation in juicebox
+(juicer pre $(basename ${assembly%.*})_yahs.bin $(basename ${assembly%.*})_yahs_scaffolds_final.agp $assembly.fai |
+       	sort -k2,2d -k6,6d -T ./ --parallel=8 -S32G |
+       	awk 'NF' > alignments_sorted.txt.part) && (mv alignments_sorted.txt.part alignments_sorted.txt)
 
 # deactivate software
 conda deactivate
@@ -130,6 +134,7 @@ module load java-uoneasy/17.0.6
 java -jar -Xmx64G ~/software_bin/juicer/juicer_tools_1.22.01.jar pre \
 	alignments_sorted.txt out.hic.part $(basename ${assembly%.*})_yahs_scaffolds_final.chrom.sizes
 mv out.hic.part out.hic
+
 module unload java-uoneasy/17.0.6
 
 
