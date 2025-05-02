6b184a7bfe577ea557f325972b47053d9e95852a
Laura Dean <lldean18@gmail.com>
2025-05-02 10:36:54 +0100

    modifying to run for hifiasm9

--- sumatran_tiger/28_scaffold_with_HiC.bash
+++ sumatran_tiger/28_scaffold_with_HiC.bash
@@ -8,8 +8,8 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=32
-#SBATCH --mem=300g
-#SBATCH --time=60:00:00
+#SBATCH --mem=50g
+#SBATCH --time=12:00:00
 #SBATCH --output=/gpfs01/home/mbzlld/code_and_scripts/slurm_out_scripts/slurm-%x-%j.out
 
 source $HOME/.bash_profile
