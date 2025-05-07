1edf8333d8efadc6e2f68edf9d7dc6d360d6ad76
Laura Dean <lldean18@gmail.com>
2025-05-07 12:54:51 +0100

    new script to run flye with the simplex only data

---
+++ sumatran_tiger/08.5_tig_flye_assembly5.bash
@@ -0,0 +1,39 @@
+#!/bin/bash
+# Laura Dean
+# 7/5/25
+# for running on Ada
+
+#SBATCH --partition=hmemq
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=96
+#SBATCH --mem=1495g
+#SBATCH --time=168:00:00
+#SBATCH --job-name=tig_flye5
+#SBATCH --output=/gpfs01/home/mbzlld/code_and_scripts/slurm_out_scripts/slurm-%x-%j.out
+
+# load your bash environment for using conda
+source $HOME/.bash_profile
+
+# set variables
+species=sumatran_tiger # set the species
+wkdir=/gpfs01/home/mbzlld/data/OrgOne/${species} # set the working directory
+try=5
+
+## create a conda environment and install the software you want
+#conda create --name flye -c conda-forge -c bioconda flye
+
+# activate the conda environment
+conda activate flye
+
+# assemble your genome from fastq files (using all pass and fail reads)
+flye \
+--threads 96 \
+--iterations 5 \
+-o $wkdir/flye_asm${try} \
+--nano-hq $wkdir/basecalls/all_simplex_simplex.fastq.gz \
+--min-overlap 10000
+
+# deactivate the conda environment
+conda deactivate
+
