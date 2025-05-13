7f35b4971985113ea123f198e603fa2f209b31cb
Laura Dean <lldean18@gmail.com>
2025-05-13 11:51:44 +0100

    updating for the new simplex only plot

--- sumatran_tiger/27_genespace_plot.bash
+++ sumatran_tiger/27_genespace_plot.bash
@@ -30,6 +30,7 @@ wkdir=~/data/OrgOne/sumatran_tiger/genespace_ours7
 wkdir=~/data/OrgOne/sumatran_tiger/genespace_ours8
 wkdir=~/data/OrgOne/sumatran_tiger/genespace_ours9
 wkdir=~/data/OrgOne/sumatran_tiger/genespace_ours10
+wkdir=~/data/OrgOne/sumatran_tiger/genespace_ours11
 
 
 
@@ -42,36 +43,30 @@ mkdir $wkdir/peptide
 
 # copy my bed files to the bed folder, stripping all columns after the 4th column when copying
 # This was required for the data to load correctly
-#cut -f1-4 ~/data/OrgOne/sumatran_tiger/raft_hifiasm_asm9/finalasm.bp.p_ctg_liftoff_genes.bed > $wkdir/bed/RaftHifiasmAsm9.bed
-cut -f1-4 ~/data/OrgOne/sumatran_tiger/raft_hifiasm_asm10/finalasm.bp.p_ctg_100kb_liftoff_genes.bed > $wkdir/bed/RaftHifiasmAsm10.bed
-cut -f1-4 ~/data/OrgOne/sumatran_tiger/hifiasm_asm10/ONTasm.bp.p_ctg_100kb_liftoff_genes.bed > $wkdir/bed/hifiasm10.bed
-cut -f1-4 ~/data/OrgOne/sumatran_tiger/liger_reference/GCA_018350195.2_chrs_only_uniq_names_nospaces_liftoff_genes.bed > $wkdir/bed/LigerHaplomeHiC.bed
-cut -f1-4 ~/data/OrgOne/sumatran_tiger/liger_reference/GCA_018350195.2_scaff_only_names_split_contigs_liftoff_genes.bed > $wkdir/bed/LigerHaplome.bed
-cut -f1-4 ~/data/OrgOne/sumatran_tiger/domestic_cat_reference/AnAms1.0.genome_genes.bed > $wkdir/bed/DomesticCat.bed
-cut -f1-4 ~/data/OrgOne/sumatran_tiger/sumatran_tiger_flye_asm4/assembly_liftoff_genes.bed > $wkdir/bed/Flye4.bed
-cut -f1-4 ~/data/OrgOne/sumatran_tiger/hifiasm_asm11/ONTasm.bp.p_ctg_100kb_liftoff_genes.bed > $wkdir/bed/hifiasm11.bed
-cut -f1-4 ~/data/OrgOne/sumatran_tiger/domestic_cat_reference/AnAms1.0.genome_split_contigs_liftoff_genes.bed > $wkdir/bed/DomesticCatContig.bed
-#cut -f1-4 ~/data/OrgOne/sumatran_tiger/HiC/ONTasm.bp.p_ctg_100kb_yahs_scaffolds_final_liftoff_genes.bed > $wkdir/bed/hifiasm10HiC.bed
-cut -f1-4 ~/data/OrgOne/sumatran_tiger/HiC/ONTasm.bp.p_ctg_100kb_yahs_scaffolds_final_ragtag/ragtag.scaffold_liftoff_genes.bed > $wkdir/bed/hifiasm10HiC.bed
-cut -f1-4 ~/data/OrgOne/sumatran_tiger/HiC2/ONTasm.bp.p_ctg_100kb_yahs_scaffolds_final_ragtag/ragtag.scaffold_liftoff_genes.bed > $wkdir/bed/hifiasm9HiC.bed
-cut -f1-4 ~/data/OrgOne/sumatran_tiger/hifiasm_asm9/ONTasm.bp.p_ctg_100kb_liftoff_genes.bed > $wkdir/bed/hifiasm9.bed
+cut -f1-4 ~/data/OrgOne/sumatran_tiger/raft_hifiasm_asm12/finalasm.bp.p_ctg_100kb_liftoff_genes.bed > $wkdir/bed/HERRO-RAFT-hifiasm.bed
+cut -f1-4 ~/data/OrgOne/sumatran_tiger/hifiasm_asm9/ONTasm.bp.p_ctg_100kb_liftoff_genes.bed > $wkdir/bed/hifiasmONT.bed
+cut -f1-4 ~/data/OrgOne/sumatran_tiger/liger_reference/GCA_018350195.2_chrs_only_uniq_names_nospaces_liftoff_genes.bed > $wkdir/bed/TigerScaffolded.bed
+cut -f1-4 ~/data/OrgOne/sumatran_tiger/liger_reference/GCA_018350195.2_scaff_only_names_split_contigs_liftoff_genes.bed > $wkdir/bed/Tiger.bed
+cut -f1-4 ~/data/OrgOne/sumatran_tiger/domestic_cat_reference/AnAms1.0.genome_genes.bed > $wkdir/bed/DomesticCatScaffolded.bed
+cut -f1-4 ~/data/OrgOne/sumatran_tiger/flye_asm5/assembly_100kb_liftoff_genes.bed > $wkdir/bed/Flye.bed
+cut -f1-4 ~/data/OrgOne/sumatran_tiger/hifiasm_asm11/ONTasm.bp.p_ctg_100kb_liftoff_genes.bed > $wkdir/bed/HifiasmDuplex.bed
+cut -f1-4 ~/data/OrgOne/sumatran_tiger/domestic_cat_reference/AnAms1.0.genome_split_contigs_liftoff_genes.bed > $wkdir/bed/DomesticCat.bed
+#cut -f1-4 ~/data/OrgOne/sumatran_tiger/HiC/ONTasm.bp.p_ctg_100kb_yahs_scaffolds_final_ragtag/ragtag.scaffold_liftoff_genes.bed > $wkdir/bed/hifiasm10HiC.bed
+cut -f1-4 ~/data/OrgOne/sumatran_tiger/HiC2/ONTasm.bp.p_ctg_100kb_yahs_scaffolds_final_ragtag/ragtag.scaffold_liftoff_genes.bed > $wkdir/bed/hifiasmONTscaffolded.bed
 
 
 # copy my protein fasta files
 # currently erroring because there are . characters in the fasta sequence and these aren't allowed by diamond
-#cp ~/data/OrgOne/sumatran_tiger/raft_hifiasm_asm9/finalasm.bp.p_ctg_proteins.fasta $wkdir/peptide/RaftHifiasmAsm9.fa
-cp ~/data/OrgOne/sumatran_tiger/raft_hifiasm_asm10/finalasm.bp.p_ctg_100kb_proteins.fasta $wkdir/peptide/RaftHifiasmAsm10.fa
-cp ~/data/OrgOne/sumatran_tiger/hifiasm_asm10/ONTasm.bp.p_ctg_100kb_proteins.fasta $wkdir/peptide/hifiasm10.fa
-cp ~/data/OrgOne/sumatran_tiger/liger_reference/GCA_018350195.2_chrs_only_uniq_names_nospaces_proteins.fasta $wkdir/peptide/LigerHaplomeHiC.fa
-cp ~/data/OrgOne/sumatran_tiger/liger_reference/GCA_018350195.2_scaff_only_names_split_contigs_proteins.fasta $wkdir/peptide/LigerHaplome.fa
+cp ~/data/OrgOne/sumatran_tiger/raft_hifiasm_asm12/finalasm.bp.p_ctg_100kb_proteins.fasta $wkdir/peptide/HERRO-RAFT-hifiasm.fa
+cp ~/data/OrgOne/sumatran_tiger/hifiasm_asm9/ONTasm.bp.p_ctg_100kb_proteins.fasta $wkdir/peptide/hifiasmONT.fa
+cp ~/data/OrgOne/sumatran_tiger/liger_reference/GCA_018350195.2_chrs_only_uniq_names_nospaces_proteins.fasta $wkdir/peptide/TigerScaffolded.fa
+cp ~/data/OrgOne/sumatran_tiger/liger_reference/GCA_018350195.2_scaff_only_names_split_contigs_proteins.fasta $wkdir/peptide/Tiger.fa
 cp ~/data/OrgOne/sumatran_tiger/domestic_cat_reference/AnAms1.0.genome_proteins.fasta $wkdir/peptide/DomesticCat.fa
-cp ~/data/OrgOne/sumatran_tiger/sumatran_tiger_flye_asm4/assembly_proteins.fasta $wkdir/peptide/Flye4.fa
-cp ~/data/OrgOne/sumatran_tiger/hifiasm_asm11/ONTasm.bp.p_ctg_100kb_proteins.fasta $wkdir/peptide/hifiasm11.fa
-cp ~/data/OrgOne/sumatran_tiger/domestic_cat_reference/AnAms1.0.genome_split_contigs_proteins.fasta $wkdir/peptide/DomesticCatContig.fa
-#cp ~/data/OrgOne/sumatran_tiger/HiC/ONTasm.bp.p_ctg_100kb_yahs_scaffolds_final_proteins.fasta $wkdir/peptide/hifiasm10HiC.fa
-cp ~/data/OrgOne/sumatran_tiger/HiC/ONTasm.bp.p_ctg_100kb_yahs_scaffolds_final_ragtag/ragtag.scaffold_proteins.fasta $wkdir/peptide/hifiasm10HiC.fa
-cp ~/data/OrgOne/sumatran_tiger/HiC2/ONTasm.bp.p_ctg_100kb_yahs_scaffolds_final_ragtag/ragtag.scaffold_proteins.fasta $wkdir/peptide/hifiasm9HiC.fa
-cp ~/data/OrgOne/sumatran_tiger/hifiasm_asm9/ONTasm.bp.p_ctg_100kb_proteins.fasta $wkdir/peptide/hifiasm9.fa
+cp ~/data/OrgOne/sumatran_tiger/flye_asm5/assembly_100kb_proteins.fasta $wkdir/peptide/Flye.fa
+cp ~/data/OrgOne/sumatran_tiger/hifiasm_asm11/ONTasm.bp.p_ctg_100kb_proteins.fasta $wkdir/peptide/HifiasmDuplex.fa
+cp ~/data/OrgOne/sumatran_tiger/domestic_cat_reference/AnAms1.0.genome_split_contigs_proteins.fasta $wkdir/peptide/DomesticCat.fa
+#cp ~/data/OrgOne/sumatran_tiger/HiC/ONTasm.bp.p_ctg_100kb_yahs_scaffolds_final_ragtag/ragtag.scaffold_proteins.fasta $wkdir/peptide/hifiasm10HiC.fa
+cp ~/data/OrgOne/sumatran_tiger/HiC2/ONTasm.bp.p_ctg_100kb_yahs_scaffolds_final_ragtag/ragtag.scaffold_proteins.fasta $wkdir/peptide/hifiasmONTscaffolded.fa
 
 
 
