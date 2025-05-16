4a9057bce3717b942e68ff7813e79727fbb724b9
Laura Dean <lldean18@gmail.com>
2025-05-16 09:47:17 +0100

    adding script to scaffold assemblies based on a reference with ragtag. Scaffolding is working, not sure if subsequent filtering is running how I want it yet

---
+++ sumatran_tiger/34_scaffold_with_ragtag.bash
@@ -0,0 +1,57 @@
+#!/bin/bash
+# Laura Dean
+# 15/5/25
+# script written for running on the UoN HPC Ada
+
+#SBATCH --job-name=ragtag_scaffold
+#SBATCH --partition=defq
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=30g
+#SBATCH --time=10:00:00
+#SBATCH --output=/gpfs01/home/mbzlld/code_and_scripts/slurm_out_scripts/slurm-%x-%j.out
+
+# set variables
+reference=/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/liger_reference/GCA_018350195.2_chrs_only_uniq_names_nospaces.fasta
+assembly=/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/raft_hifiasm_asm12/finalasm.bp.p_ctg_100kb.fasta
+
+
+# scaffold assembly with ragtag
+source $HOME/.bash_profile
+#conda activate ragtag
+#ragtag.py scaffold -t 16 -o ${assembly%.*}_ragtag $reference $assembly
+#conda deactivate
+
+
+## get rid of the ragtag suffixes
+#sed -i 's/_RagTag//' ${assembly%.*}_ragtag/ragtag.scaffold.fasta
+
+# set the chromosome names to keep
+keep=("A1
+A2
+A3
+B1
+B2
+B3
+B4
+C1
+C2
+D1
+D2
+D3
+D4
+E1
+E2
+E3
+F2
+F3
+X")
+
+# rename scaffolds and remove unplaced contigs
+conda activate seqtk
+seqtk subseq ${assembly%.*}_ragtag/ragtag.scaffold.fasta $keep > ${assembly%.*}_ragtag/ragtag.scaffolds_only.fasta
+conda deactivate
+
+
+
