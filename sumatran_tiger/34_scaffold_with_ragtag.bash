c9aac0939f2b17fb5895442b2cf1b48028b29016
Laura Dean <lldean18@gmail.com>
2025-05-16 14:22:11 +0100

    running on simplex only flye assembly

--- sumatran_tiger/34_scaffold_with_ragtag.bash
+++ sumatran_tiger/34_scaffold_with_ragtag.bash
@@ -9,23 +9,23 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=16
 #SBATCH --mem=30g
-#SBATCH --time=10:00:00
+#SBATCH --time=12:00:00
 #SBATCH --output=/gpfs01/home/mbzlld/code_and_scripts/slurm_out_scripts/slurm-%x-%j.out
 
 # set variables
 reference=/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/liger_reference/GCA_018350195.2_chrs_only_uniq_names_nospaces.fasta
 assembly=/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/raft_hifiasm_asm12/finalasm.bp.p_ctg_100kb.fasta
-
+assembly=/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/flye_asm5/assembly_100kb.fasta
 
 # scaffold assembly with ragtag
 source $HOME/.bash_profile
-#conda activate ragtag
-#ragtag.py scaffold -t 16 -o ${assembly%.*}_ragtag $reference $assembly
-#conda deactivate
+conda activate ragtag
+ragtag.py scaffold -t 16 -o ${assembly%.*}_ragtag $reference $assembly
+conda deactivate
 
 
-## get rid of the ragtag suffixes
-#sed -i 's/_RagTag//' ${assembly%.*}_ragtag/ragtag.scaffold.fasta
+# get rid of the ragtag suffixes
+sed -i 's/_RagTag//' ${assembly%.*}_ragtag/ragtag.scaffold.fasta
 
 # set the file containing chromosome names to keep
 keep=/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/liger_reference/tiger_chrs.txt
