e4800cfa10b78c2025e786739a6eaea1aea66f09
Laura Dean <lldean18@gmail.com>
2025-05-21 17:34:37 +0100

    backing up inversion correcting for genespace

--- sumatran_tiger/27.3_customise_genespace_plot.R
+++ sumatran_tiger/27.3_customise_genespace_plot.R
@@ -30,6 +30,7 @@ out <- run_genespace(gpar, overwrite = T)
 #load('/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/genespace_ours8/results/gsParams.rda', verbose = TRUE)
 #load('/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/genespace_ours9/results/gsParams.rda', verbose = TRUE)
 load('/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/genespace_ours10/results/gsParams.rda', verbose = TRUE)
+load('/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/genespace_ours11/results/gsParams.rda', verbose = TRUE)
 
 
 
@@ -50,6 +51,56 @@ invchr <- data.frame(
           "contig_50","contig_67","contig_68","contig_60","contig_321","contig_354","contig_2","contig_1","contig_228","contig_343","contig_339","contig_687","contig_25","contig_685","contig_248","contig_163","contig_393","contig_276","contig_667","contig_555","contig_617","contig_139","contig_165","contig_271","contig_674","contig_227","contig_116",
 	  "scaffold_3", "scaffold_12",
 	  "ptg000022l","ptg000010l","ptg000004l","ptg000019l","ptg000007l","ptg000008l","ptg000002l","ptg000024l","ptg000018l","ptg000013l","ptg000009l","ptg000016l"))
+
+
+
+
+
+
+
+
+
+
+
+# set the chromosomes to be plotted inverted
+invchr <- data.frame(
+  genome = c("hifiasm10", "hifiasm10", "hifiasm10", "hifiasm10", "hifiasm10", "hifiasm10", "hifiasm10", "hifiasm10", "hifiasm10", "hifiasm10", "hifiasm10", "hifiasm10", "hifiasm10", "hifiasm10", "hifiasm10", "hifiasm10", "hifiasm10","hifiasm10","hifiasm10", "hifiasm10","hifiasm10","hifiasm10",
+             "Tiger","Tiger","Tiger","Tiger","Tiger","Tiger","Tiger","Tiger","Tiger","Tiger","Tiger","Tiger","Tiger","Tiger","Tiger","Tiger","Tiger",
+             "HERRORAFThifiasm","HERRORAFThifiasm","HERRORAFThifiasm","HERRORAFThifiasm","HERRORAFThifiasm","HERRORAFThifiasm","HERRORAFThifiasm","HERRORAFThifiasm","HERRORAFThifiasm","HERRORAFThifiasm","HERRORAFThifiasm","HERRORAFThifiasm","HERRORAFThifiasm","HERRORAFThifiasm","HERRORAFThifiasm","HERRORAFThifiasm","HERRORAFThifiasm","HERRORAFThifiasm","HERRORAFThifiasm","HERRORAFThifiasm","HERRORAFThifiasm","HERRORAFThifiasm",
+             "Flye","Flye","Flye","Flye","Flye","Flye","Flye","Flye","Flye"
+             "hifiasm10HiC","hifiasm10HiC",
+             "hifiasmONT","hifiasmONT","hifiasmONT","hifiasmONT","hifiasmONT","hifiasmONT","hifiasmONT","hifiasmONT","hifiasmONT","hifiasmONT","hifiasmONT","hifiasmONT"),
+  chr = c("ptg000003l","ptg000005l","ptg000006l","ptg000007l","ptg000010l","ptg000011l","ptg000013l","ptg000014l","ptg000017l","ptg000019l","ptg000021l","ptg000022l","ptg000023l","ptg000024l","ptg000026l","ptg000027l","ptg000029l","ptg000030l","ptg000031l","ptg000032l","ptg000033l","ptg000046l",
+          "Contig9","Contig29","Contig31","Contig33","Contig34","Contig35","Contig36","Contig25","Contig26","Contig27","Contig3","Contig23","Contig52","Contig38","Contig13","Contig46","Contig53",
+          "ptg000005l","ptg000017l","ptg000035l","ptg000015l","ptg000001l","ptg000019l","ptg000034l","ptg000029l","ptg000024l","ptg000012l","ptg000007l","ptg000018l","ptg000006l","ptg000002l","ptg000014l","ptg000010l","ptg000016l","ptg000023l","ptg000025l","ptg000011l","ptg000022l","ptg000033l",
+          "contig_127","contig_554","contig_214","contig_299","contig_308","contig_565","contig_167","contig_38","contig_222",
+	  "scaffold_3", "scaffold_12",
+	  "ptg000022l","ptg000010l","ptg000004l","ptg000019l","ptg000007l","ptg000008l","ptg000002l","ptg000024l","ptg000018l","ptg000013l","ptg000009l","ptg000016l"))
+
+
+
+
+#############################
+# Main plot for MS
+
+ripDat <- plot_riparian(
+  gsParam = gsParam,
+  pdfFile = paste(gsParam$paths$riparian, "/MS_plot.rip.pdf", sep = ""),
+  refGenome = "DomesticCat",
+  syntenyWeight = 1,
+  minChrLen2plot = 0,
+  xlabel = NULL,
+  labelTheseGenomes = c("hifiasmONT","Tiger","HERRORAFThifiasm","Flye"),
+  invertTheseChrs = invchr,
+  chrLabFun = function(x) gsub("^0", "", gsub("^anams1.0|chr|scaf|contig|chromosome|scaffold|^lg|_|^ptg000|l$", "", tolower(x))),
+  genomeIDs = c("Flye","HERRORAFThifiasm","hifiasmONT", "Tiger", "DomesticCat"),
+  forceRecalcBlocks = FALSE)
+
+
+
+
+
+
 
 
 
