79d72dd99d7addd8f963e450e5ae993bd357224d
Laura Dean <lldean18@gmail.com>
2025-05-29 17:42:01 +0100

    updating genespace plot

--- sumatran_tiger/27.3_customise_genespace_plot.R
+++ sumatran_tiger/27.3_customise_genespace_plot.R
@@ -69,13 +69,13 @@ invchr <- data.frame(
   genome = c("hifiasm10", "hifiasm10", "hifiasm10", "hifiasm10", "hifiasm10", "hifiasm10", "hifiasm10", "hifiasm10", "hifiasm10", "hifiasm10", "hifiasm10", "hifiasm10", "hifiasm10", "hifiasm10", "hifiasm10", "hifiasm10", "hifiasm10","hifiasm10","hifiasm10", "hifiasm10","hifiasm10","hifiasm10",
              "Tiger","Tiger","Tiger","Tiger","Tiger","Tiger","Tiger","Tiger","Tiger","Tiger","Tiger","Tiger","Tiger","Tiger","Tiger","Tiger","Tiger",
              "HERRORAFThifiasm","HERRORAFThifiasm","HERRORAFThifiasm","HERRORAFThifiasm","HERRORAFThifiasm","HERRORAFThifiasm","HERRORAFThifiasm","HERRORAFThifiasm","HERRORAFThifiasm","HERRORAFThifiasm","HERRORAFThifiasm","HERRORAFThifiasm","HERRORAFThifiasm","HERRORAFThifiasm","HERRORAFThifiasm","HERRORAFThifiasm","HERRORAFThifiasm","HERRORAFThifiasm","HERRORAFThifiasm","HERRORAFThifiasm","HERRORAFThifiasm","HERRORAFThifiasm",
-             "Flye","Flye","Flye","Flye","Flye","Flye","Flye","Flye","Flye","Flye","Flye","Flye","Flye","Flye","Flye","Flye","Flye","Flye"
+             "Flye","Flye","Flye","Flye","Flye","Flye","Flye","Flye","Flye","Flye","Flye","Flye","Flye","Flye","Flye","Flye","Flye","Flye","Flye","Flye",
              "hifiasm10HiC","hifiasm10HiC",
              "hifiasmONT","hifiasmONT","hifiasmONT","hifiasmONT","hifiasmONT","hifiasmONT","hifiasmONT","hifiasmONT","hifiasmONT","hifiasmONT","hifiasmONT","hifiasmONT"),
   chr = c("ptg000003l","ptg000005l","ptg000006l","ptg000007l","ptg000010l","ptg000011l","ptg000013l","ptg000014l","ptg000017l","ptg000019l","ptg000021l","ptg000022l","ptg000023l","ptg000024l","ptg000026l","ptg000027l","ptg000029l","ptg000030l","ptg000031l","ptg000032l","ptg000033l","ptg000046l",
           "Contig9","Contig29","Contig31","Contig33","Contig34","Contig35","Contig36","Contig25","Contig26","Contig27","Contig3","Contig23","Contig52","Contig38","Contig13","Contig46","Contig53",
           "ptg000005l","ptg000017l","ptg000035l","ptg000015l","ptg000001l","ptg000019l","ptg000034l","ptg000029l","ptg000024l","ptg000012l","ptg000007l","ptg000018l","ptg000006l","ptg000002l","ptg000014l","ptg000016l","ptg000023l","ptg000025l","ptg000011l","ptg000022l","ptg000033l","ptg000030l",
-          "contig_127","contig_554","contig_214","contig_299","contig_308","contig_565","contig_167","contig_38","contig_322","contig_5","contig_53","contig_55","contig_67","contig_595","contig_285","contig_641","contig_215","contig_248",
+          "contig_127","contig_554","contig_214","contig_299","contig_308","contig_565","contig_167","contig_38","contig_322","contig_5","contig_53","contig_55","contig_67","contig_595","contig_285","contig_641","contig_215","contig_248","contig_681","contig_450",
 	  "scaffold_3", "scaffold_12",
 	  "ptg000022l","ptg000010l","ptg000004l","ptg000019l","ptg000007l","ptg000008l","ptg000002l","ptg000024l","ptg000018l","ptg000013l","ptg000009l","ptg000016l"))
 
@@ -95,12 +95,27 @@ ripDat <- plot_riparian(
   invertTheseChrs = invchr,
   chrLabFun = function(x) gsub("^0", "", gsub("^anams1.0|chr|scaf|contig|chromosome|scaffold|^lg|_|^ptg000|l$", "", tolower(x))),
   genomeIDs = c("Flye","HERRORAFThifiasm","hifiasmONT", "Tiger", "DomesticCat"),
-  forceRecalcBlocks = FALSE,
-  addThemes = ggthemes)
+  forceRecalcBlocks = FALSE)
+
+#,
+#  addThemes = ggthemes
 
 
+#############################
+# PLot for supp mat
 
-
+ripDat <- plot_riparian(
+  gsParam = gsParam,
+  pdfFile = paste(gsParam$paths$riparian, "/MS_plot_supp_duplex.rip.pdf", sep = ""),
+  refGenome = "DomesticCat",
+  syntenyWeight = 1,
+  minChrLen2plot = 0,
+  xlabel = NULL,
+  labelTheseGenomes = c("hifiasmONT","Tiger","HERRORAFThifiasm","Flye"),
+  invertTheseChrs = invchr,
+  chrLabFun = function(x) gsub("^0", "", gsub("^anams1.0|chr|scaf|contig|chromosome|scaffold|^lg|_|^ptg000|l$", "", tolower(x))),
+  genomeIDs = c("HifiasmDuplex","Flye","HERRORAFThifiasm","hifiasmONT", "Tiger", "DomesticCat"),
+  forceRecalcBlocks = FALSE)
 
 
 
