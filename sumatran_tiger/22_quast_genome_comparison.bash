8912c17a694e223f74f3d2fbe0586967a77afb0b
Laura Dean <lldean18@gmail.com>
2025-08-20 11:31:17 +0100

    updating quast script to run with the liger asm as ref

--- sumatran_tiger/22_quast_genome_comparison.bash
+++ sumatran_tiger/22_quast_genome_comparison.bash
@@ -14,9 +14,12 @@
 #SBATCH --output=/gpfs01/home/mbzlld/code_and_scripts/slurm_out_scripts/slurm-%x-%j.out
 
 # set variables
-#reference=/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/liger_reference/GCA_018350195.2_chrs_only_uniq_names_nospaces.fasta
-reference=/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/domestic_cat_reference/AnAms1.0.genome.fa
-annotation=/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/domestic_cat_reference/AnAms1.0r1.0.2.gff
+reference=/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/liger_reference/GCA_018350195.2_chrs_only_uniq_names_nospaces.fasta
+annotation=/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/liger_reference/GCA_018350195.2_chrs_only_uniq_names_nospaces_liftoff.gff
+
+#reference=/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/domestic_cat_reference/AnAms1.0.genome.fa
+#annotation=/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/domestic_cat_reference/AnAms1.0r1.0.2.gff
+
 wkdir=/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger
 
 
@@ -31,8 +34,26 @@ python /gpfs01/home/mbzlld/software_bin/miniconda3/envs/quast/bin/quast \
 	--eukaryote \
 	-r $reference \
 	-g $annotation \
-	-o $wkdir/quast6 $wkdir/liger_reference/GCA_018350195.2_scaff_only_names_split_contigs_100kb.fasta $wkdir/hifiasm_asm9/ONTasm.bp.p_ctg_100kb.fasta $wkdir/HiC2/ONTasm.bp.p_ctg_100kb_yahs_scaffolds_final_ragtag/ragtag.scaffold.fasta $wkdir/hifiasm_asm11/ONTasm.bp.p_ctg_100kb.fasta $wkdir/raft_hifiasm_asm12/finalasm.bp.p_ctg_100kb.fasta $wkdir/flye_asm5/assembly_100kb.fasta $wkdir/hifiasm_asm13/ONTasm.bp.p_ctg_100kb.fasta
+	-o $wkdir/quast7 \
+	$wkdir/domestic_cat_reference/AnAms1.0.genome.fa \
+	$wkdir/domestic_cat_reference/AnAms1.0.genome_split_contigs.fa \
+	$wkdir/liger_reference/GCA_018350195.2_scaff_only_names_split_contigs_100kb.fasta \
+	$wkdir/hifiasm_asm9/ONTasm.bp.p_ctg_100kb.fasta \
+	$wkdir/HiC2/ONTasm.bp.p_ctg_100kb_yahs_scaffolds_final_ragtag/ragtag.scaffold.fasta \
+	$wkdir/hifiasm_asm11/ONTasm.bp.p_ctg_100kb.fasta \
+	$wkdir/raft_hifiasm_asm12/finalasm.bp.p_ctg_100kb.fasta \
+	$wkdir/flye_asm5/assembly_100kb.fasta \
+	$wkdir/hifiasm_asm13/ONTasm.bp.p_ctg_100kb.fasta
 
+# scaffolded cat
+# unscaffolded cat
+# unscaffolded liger
+# HIFIASM ONT
+# HiC scaffolded asm
+# Hifiasm duplex with hifiasm no ONT
+# HERRO_RAFT_HIFIASM asm
+# Flye ASM
+# simplex only asm with hifiasm no ONT
 
 conda deactivate
 
