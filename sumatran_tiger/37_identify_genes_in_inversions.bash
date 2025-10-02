8c481bc79f1880b8fe559c19dc44262f3ca9cb2b
Laura Dean <lldean18@gmail.com>
2025-10-02 11:19:57 +0100

    updating run time and memory

--- sumatran_tiger/37_identify_genes_in_inversions.bash
+++ sumatran_tiger/37_identify_genes_in_inversions.bash
@@ -8,8 +8,8 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=30g
-#SBATCH --time=5:00:00
+#SBATCH --mem=5g
+#SBATCH --time=1:00:00
 #SBATCH --output=/gpfs01/home/mbzlld/code_and_scripts/slurm_out_scripts/slurm-%x-%j.out
 
 
