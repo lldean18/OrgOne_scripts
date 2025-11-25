8816b3a0933426933767d484203ca69590f3c096
Laura Dean <lldean18@gmail.com>
2025-11-25 14:51:16 +0000

    final annotated version of what I did

--- ctenella/00_copy_data_from_prom.bash
+++ ctenella/00_copy_data_from_prom.bash
@@ -1,4 +1,8 @@
+#!/bin/bash
+# Laura Dean
+# 25/11/25
 
+# noting down the code I used to copy the files from prom to Ada
 
 # setup tmux session
 conda activate tmux
@@ -7,4 +11,10 @@ tmux attach
 # request an interactive job
 srun --partition defq --cpus-per-task 1 --mem 50g --time 168:00:00 --pty bash
 
+# change to dir in which I want to put the data
+cd /gpfs01/home/mbzlld/data/ctenella/pod5s
+
+# copy the data to Ada
 rsync -rvh --progress prom@10.157.252.34:/data/ic_213 ./
+
+
