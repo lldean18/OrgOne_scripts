453ed356dfd2cb5e922f6fcfb357731863ca95b8
Laura Dean <lldean18@gmail.com>
2025-12-04 11:28:47 +0000

    adding modifications to code that ran successfully on oldprom in the end

--- ctenella/03_basecall_newprom.bash
+++ ctenella/03_basecall_newprom.bash
@@ -1,10 +1,12 @@
 #!/bin/bash
 # Laura Dean
 # file written for running on newprom
-# 2/12/25
+# 4/12/25
 
 # script to basecall the raw traces from pod5 files
 
+# attach to my tmux session
+tmux a -t laura
 
 # make a directory for the basecalled files (only if it does not already exist)
 mkdir -p /data/laura/basecalls
@@ -13,13 +15,12 @@ mkdir -p /data/laura/basecalls
 
 # basecall the simplex reads
 dorado basecaller \
-	sup@latest,5mCG_5hmCG \
+	/data/laura/dorado_models/dna_r10.4.1_e8.2_400bps_sup@v5.2.0 \
 	--recursive \
-       	/data/laura/pod5s > /data/laura/basecalls/SUP_calls.bam
+       	/data/laura/ic_213 > /data/laura/basecalls/SUP_calls.bam
 
 
 #	sup@latest,5mCG_5hmCG \
-#	/gpfs01/home/mbzlld/data/dorado_models/dna_r10.4.1_e8.2_400bps_sup@v5.2.0 \
 
 
 
