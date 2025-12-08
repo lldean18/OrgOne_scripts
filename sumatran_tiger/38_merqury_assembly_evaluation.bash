e60107c4956027f9b54cb0ec63fb965278f4441a
Laura Dean <lldean18@gmail.com>
2025-12-08 13:56:10 +0000

    running for the contig level assembly to see if its similar to the homology scaffolded one

--- sumatran_tiger/38_merqury_assembly_evaluation.bash
+++ sumatran_tiger/38_merqury_assembly_evaluation.bash
@@ -13,13 +13,18 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=65
 #SBATCH --mem=200g
-#SBATCH --time=168:00:00
+#SBATCH --time=48:00:00
 #SBATCH --output=/gpfs01/home/mbzlld/code_and_scripts/slurm_out_scripts/slurm-%x-%j.out
 
 
+## setup environment
+#cd /gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/hifiasm_asm9/ONTasm.bp.p_ctg_100kb_ragtag
+#assembly=/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/hifiasm_asm9/ONTasm.bp.p_ctg_100kb_ragtag/ragtag.scaffolds_only.fasta
+#duplex=/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/basecalls/all_extracted_duplex_duplex.fastq.gz
+
 # setup environment
-wkdir=/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger
-assembly=/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/hifiasm_asm9/ONTasm.bp.p_ctg_100kb_ragtag/ragtag.scaffolds_only.fasta
+cd /gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/hifiasm_asm9
+assembly=/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/hifiasm_asm9/ONTasm.bp.p_ctg_100kb.fasta
 duplex=/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/basecalls/all_extracted_duplex_duplex.fastq.gz
 
 source $HOME/.bash_profile
@@ -30,12 +35,25 @@ conda activate merqury
 ### prepare the meryl database ###
 ##################################
 
-# estimate best k-mer size (by giving genome size in bp)
-bash $MERQURY/best_k.sh 2451351749
-# suggests k= 20.6 but the tool suggests in most cases k=31 is optimal so will begin with that
+# # estimate best k-mer size (by giving genome size in bp)
+# bash $MERQURY/best_k.sh 2451351749
+# # suggests k= 20.6 but the tool suggests in most cases k=31 is optimal so will begin with that
 
-# build the database
-meryl k=31 count $duplex output ${duplex%.*.*}.meryl
+# # build the database
+# meryl k=31 count $duplex output ${duplex%.*.*}.meryl
+
+
+
+#######################################
+### perform k-mer genome evaluation ###
+#######################################
+
+$MERQURY/merqury.sh \
+${duplex%.*.*}.meryl \
+$assembly \
+meryl
+
+
 
 
 
