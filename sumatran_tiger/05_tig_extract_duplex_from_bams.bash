0a301bb6b700605730fc37543bfffe2af05ad06f
Laura Dean <lldean18@gmail.com>
2025-12-08 10:26:08 +0000

    modified as I thought I hadn't extracted the duplex reads and I needed them, but then found them so didnt actually run this again

--- sumatran_tiger/05_tig_extract_duplex_from_bams.bash
+++ sumatran_tiger/05_tig_extract_duplex_from_bams.bash
@@ -39,24 +39,24 @@ bam=$(awk -v ArrayTaskID=$SLURM_ARRAY_TASK_ID '$1==ArrayTaskID {print $2}' $conf
 # load modules
 module load samtools-uoneasy/1.18-GCC-12.3.0
 
-# # extract duplex reads from bam files
-# samtools view \
-# --tag dx:1 \
-# --threads 48 \
-# -O bam \
-# --write-index \
-# --output ${bam%.*}_duplex.bam \
-# $bam
-
-# extract simplex reads from bam files
+# extract duplex reads from bam files
 samtools view \
---tag dx:0 \
---tag dx:-1 \
+--tag dx:1 \
 --threads 48 \
 -O bam \
 --write-index \
---output ${bam%.*}_simplex.bam \
+--output ${bam%.*}_duplex.bam \
 $bam
+
+# # extract simplex reads from bam files
+# samtools view \
+# --tag dx:0 \
+# --tag dx:-1 \
+# --threads 48 \
+# -O bam \
+# --write-index \
+# --output ${bam%.*}_simplex.bam \
+# $bam
 
 
 
