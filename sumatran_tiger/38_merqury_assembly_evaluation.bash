90172538045bd9bb27359812810d863ef40946e3
Laura Dean <lldean18@gmail.com>
2025-12-08 10:03:36 +0000

    beginning my merqury script

---
+++ sumatran_tiger/38_merqury_assembly_evaluation.bash
@@ -0,0 +1,41 @@
+#!/bin/bash
+# Laura Dean
+# 8/12/25
+# script written for running on the UoN HPC Ada
+
+# script to run the merqury tool on a genome assembly to assess quality.
+# the tool is designed to work with an assembly and illumnina data but
+# we dont have short reads so seeing how it works with duplex reads instead.
+
+#SBATCH --job-name=merqury
+#SBATCH --partition=defq
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=30g
+#SBATCH --time=24:00:00
+#SBATCH --output=/gpfs01/home/mbzlld/code_and_scripts/slurm_out_scripts/slurm-%x-%j.out
+
+
+# setup environment
+wkdir=
+assembly=/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/hifiasm_asm9/ONTasm.bp.p_ctg_100kb_ragtag/ragtag.scaffolds_only.fasta
+duplex=
+
+#conda create --name merqury -c conda-forge -c bioconda merqury -y
+conda activate merqury
+
+##################################
+### prepare the meryl database ###
+##################################
+
+# estimate best k-mer size (by giving genome size in bp)
+bash $MERQURY/best_k.sh 2451351749
+# suggests k= 20.6 but the tool suggests in most cases k=31 is optimal so will begin with that
+
+# build the database
+meryl k=31 count $duplex output ${duplex%.*}
+
+
+
+
