033845e13652e5afb2bfe90e36e32252ecd95b0e
Laura Dean <lldean18@gmail.com>
2025-12-11 12:08:34 +0000

    running quast against the cat reference

--- sumatran_tiger/22_quast_genome_comparison.bash
+++ sumatran_tiger/22_quast_genome_comparison.bash
@@ -17,11 +17,11 @@
 #SBATCH --output=/gpfs01/home/mbzlld/code_and_scripts/slurm_out_scripts/slurm-%x-%j.out
 
 # set variables
-reference=/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/liger_reference/GCA_018350195.2_chrs_only_uniq_names_nospaces.fasta
-annotation=/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/liger_reference/GCA_018350195.2_chrs_only_uniq_names_nospaces_liftoff.gff
+#reference=/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/liger_reference/GCA_018350195.2_chrs_only_uniq_names_nospaces.fasta
+#annotation=/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/liger_reference/GCA_018350195.2_chrs_only_uniq_names_nospaces_liftoff.gff
 
-#reference=/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/domestic_cat_reference/AnAms1.0.genome.fa
-#annotation=/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/domestic_cat_reference/AnAms1.0r1.0.2.gff
+reference=/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/domestic_cat_reference/AnAms1.0.genome.fa
+annotation=/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/domestic_cat_reference/AnAms1.0r1.0.2.gff
 
 wkdir=/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger
 
@@ -37,8 +37,8 @@ python /gpfs01/home/mbzlld/software_bin/miniconda3/envs/quast/bin/quast \
 	--eukaryote \
 	-r $reference \
 	-g $annotation \
-	-o $wkdir/quast8 \
-	$wkdir/domestic_cat_reference/AnAms1.0.genome.fa \
+	-o $wkdir/quast9 \
+	$wkdir/liger_reference/GCA_018350195.2_chrs_only_uniq_names_nospaces.fasta \
 	$wkdir/hifiasm_asm13/ONTasm.bp.p_ctg_100kb.fasta \
 	$wkdir/hifiasm_asm11/ONTasm.bp.p_ctg_100kb.fasta \
 	$wkdir/flye_asm5/assembly_100kb.fasta \
@@ -47,8 +47,11 @@ python /gpfs01/home/mbzlld/software_bin/miniconda3/envs/quast/bin/quast \
 	$wkdir/hifiasm_asm9/ONTasm.bp.p_ctg_100kb.fasta
 
 # scaffolded cat
+#	$wkdir/domestic_cat_reference/AnAms1.0.genome.fa \
 # unscaffolded cat
 #	$wkdir/domestic_cat_reference/AnAms1.0.genome_split_contigs.fa \
+# scaffolded liger
+	$wkdir/liger_reference/GCA_018350195.2_chrs_only_uniq_names_nospaces.fasta \
 # unscaffolded liger
 #	$wkdir/liger_reference/GCA_018350195.2_scaff_only_names_split_contigs_100kb.fasta \
 # HiC scaffolded asm
