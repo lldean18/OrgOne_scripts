3f2ffcad786db8be2fd91f842d32a1b9caa5b9a3
Laura Dean <lldean18@gmail.com>
2025-12-15 15:02:14 +0000

    adding script to filter reads for any rogue hamster reads

---
+++ ctenella/11_filter_hamster.bash
@@ -0,0 +1,64 @@
+#!/bin/bash
+# Laura Dean
+# 15/12/25
+# script written for running on the UoN HPC Ada
+
+
+# script to map reads to chinese hamster to ensure no contamination happened from reusing the flowcell
+
+
+#SBATCH --job-name=filter_hamster
+#SBATCH --partition=defq
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=64
+#SBATCH --mem=200g
+#SBATCH --time=80:00:00
+#SBATCH --output=/gpfs01/home/mbzlld/code_and_scripts/slurm_out_scripts/slurm-%x-%j.out
+
+# set variables
+wkdir=/gpfs01/home/mbzlld/data/ctenella/hamster
+reads=/gpfs01/home/mbzlld/data/ctenella/SUP_calls.fastq.gz
+
+# setup env
+cd $wkdir
+source $HOME/.bash_profile
+
+
+### map the reads to the hamster fasta
+# (retain only the reads that don't map going forward for coral assembly
+conda activate bbmap
+bbsplit.sh \
+-Xmx170G \
+fastareadlen=600 \
+ref=$wkdir/GCF_003668045.3_CriGri-PICRH-1.0_genomic.fna.gz \
+in=${reads%.*.*}.fasta.gz \
+basename=reads_mapping_to_%.fastq
+conda deactivate
+
+
+# # extract the fastq header lines (without the @)
+# awk 'NR % 4 == 1 {sub(/^@/, ""); print}' $wkdir/reads_mapping_to_hamster.fastq > $wkdir/hamster_read_ids.txt
+# # remove everything after the first space on every line to retain only the read ids
+# sed -i "s/ .*//" $wkdir/hamster_read_ids.txt
+# # remove the now duplicate lines where the same read id is identified more than once
+# awk '!seen[$0]++' $wkdir/hamster_read_ids.txt > $wkdir/temporary && mv $wkdir/temporary $wkdir/hamster_read_ids.txt
+# 
+# 
+# 
+# # filter the reads to remove these contaminants
+# conda activate seqkit
+# seqkit grep --threads 24 -v -f $wkdir/hamster_read_ids.txt $wkdir/SUP_calls.fastq.gz | gzip > $wkdir/SUP_calls_no_hamster.fastq.gz
+# conda deactivate
+# 
+# 
+# 
+# # keep the contaminanat reads in another file
+# conda activate seqkit
+# seqkit grep --threads 24 -f $wkdir/hamster_read_ids.txt $wkdir/SUP_calls.fastq.gz | gzip > $wkdir/SUP_calls_mapped_to_hamster.fastq.gz
+# conda deactivate
+# 
+# 
+# 
+
+
