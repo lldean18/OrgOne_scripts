f3b933fa09873678470520b26e9d1d204926bbd4
Laura Dean <lldean18@gmail.com>
2026-01-09 14:12:40 +0000

    adding changes from before xmas

--- ctenella/11_filter_hamster.bash
+++ ctenella/11_filter_hamster.bash
@@ -31,34 +31,35 @@ conda activate bbmap
 bbsplit.sh \
 -Xmx170G \
 fastareadlen=600 \
+minid=0.94 \
 ref=$wkdir/GCF_003668045.3_CriGri-PICRH-1.0_genomic.fna.gz \
 in=${reads%.*.*}.fasta.gz \
 basename=reads_mapping_to_%.fastq
 conda deactivate
 
 
-# # extract the fastq header lines (without the @)
-# awk 'NR % 4 == 1 {sub(/^@/, ""); print}' $wkdir/reads_mapping_to_hamster.fastq > $wkdir/hamster_read_ids.txt
-# # remove everything after the first space on every line to retain only the read ids
-# sed -i "s/ .*//" $wkdir/hamster_read_ids.txt
-# # remove the now duplicate lines where the same read id is identified more than once
-# awk '!seen[$0]++' $wkdir/hamster_read_ids.txt > $wkdir/temporary && mv $wkdir/temporary $wkdir/hamster_read_ids.txt
-# 
-# 
-# 
-# # filter the reads to remove these contaminants
-# conda activate seqkit
-# seqkit grep --threads 24 -v -f $wkdir/hamster_read_ids.txt $wkdir/SUP_calls.fastq.gz | gzip > $wkdir/SUP_calls_no_hamster.fastq.gz
-# conda deactivate
-# 
-# 
-# 
-# # keep the contaminanat reads in another file
-# conda activate seqkit
-# seqkit grep --threads 24 -f $wkdir/hamster_read_ids.txt $wkdir/SUP_calls.fastq.gz | gzip > $wkdir/SUP_calls_mapped_to_hamster.fastq.gz
-# conda deactivate
-# 
-# 
-# 
+# extract the fastq header lines (without the @)
+awk 'NR % 4 == 1 {sub(/^@/, ""); print}' $wkdir/reads_mapping_to_GCF_003668045.3_CriGri-PICRH-1.0_genomic.fastq > $wkdir/hamster_read_ids.txt
+# remove everything after the first space on every line to retain only the read ids
+sed -i "s/ .*//" $wkdir/hamster_read_ids.txt
+# remove the now duplicate lines where the same read id is identified more than once
+awk '!seen[$0]++' $wkdir/hamster_read_ids.txt > $wkdir/temporary && mv $wkdir/temporary $wkdir/hamster_read_ids.txt
+
+
+
+# filter the reads to remove these contaminants
+conda activate seqkit
+seqkit grep --threads 24 -v -f $wkdir/hamster_read_ids.txt $reads | gzip > $wkdir/SUP_calls_no_hamster.fastq.gz
+conda deactivate
+
+
+
+# keep the contaminanat reads in another file
+conda activate seqkit
+seqkit grep --threads 24 -f $wkdir/hamster_read_ids.txt $reads | gzip > $wkdir/SUP_calls_mapped_to_hamster.fastq.gz
+conda deactivate
+
+
+
 
 
