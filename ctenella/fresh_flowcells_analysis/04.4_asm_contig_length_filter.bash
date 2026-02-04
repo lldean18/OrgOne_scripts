35b4e4130c76d8c6f30a48b4e242da64690afc63
Laura Dean <lldean18@gmail.com>
2026-02-04 10:50:34 +0000

    updating to add slurm block, oops

--- ctenella/fresh_flowcells_analysis/04.4_asm_contig_length_filter.bash
+++ ctenella/fresh_flowcells_analysis/04.4_asm_contig_length_filter.bash
@@ -2,6 +2,17 @@
 # Laura Dean
 # 4/2/26
 
+#SBATCH --partition=defq
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=10g
+#SBATCH --time=00:30:00
+#SBATCH --job-name=assem_size_filt
+#SBATCH --output=/gpfs01/home/mbzlld/code_and_scripts/slurm_out_scripts/slurm-%x-%j.out
+
+
+
 # script to filter the raw ctenella reads to try an assembly with only the larger fragments
 
 # setup env
