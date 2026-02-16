5b0ffb8146a3863a6555a85ed8a9d547094492df
Laura Dean <lldean18@gmail.com>
2026-02-16 17:55:18 +0000

    trying with the broader subset of reads

--- ctenella/fresh_flowcells_analysis/05_hifiasm_assemble.bash
+++ ctenella/fresh_flowcells_analysis/05_hifiasm_assemble.bash
@@ -8,8 +8,8 @@
 #SBATCH --partition=hmemq
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=48
-#SBATCH --mem=900g
+#SBATCH --cpus-per-task=96
+#SBATCH --mem=1420g
 #SBATCH --time=80:00:00
 #SBATCH --job-name=ctenella_assembly
 #SBATCH --output=/gpfs01/home/mbzlld/code_and_scripts/slurm_out_scripts/slurm-%x-%j.out
@@ -30,6 +30,7 @@ conda activate hifiasm_0.25.0
 #wkdir=/gpfs01/home/mbzlld/data/ctenella
 #wkdir=/gpfs01/home/mbzlld/data/ctenella
 #wkdir=/gpfs01/home/mbzlld/data/ctenella
+#wkdir=/gpfs01/home/mbzlld/data/ctenella
 wkdir=/gpfs01/home/mbzlld/data/ctenella
 
 # set the attempt number for naming the output directory of each try
@@ -40,7 +41,8 @@ wkdir=/gpfs01/home/mbzlld/data/ctenella
 #attempt=5
 #attempt=6
 #attempt=7
-attempt=8
+#attempt=8
+attempt=9
 
 # then set the reads file that was used in that attempt
 #reads=$wkdir/SUP_calls.fastq.gz
@@ -50,7 +52,8 @@ attempt=8
 #reads=$wkdir/new_flowcell_calls/Ctenella_sup_36-42GC.fastq.gz
 #reads=$wkdir/new_flowcell_calls/Ctenella_sup_36-42GC.fastq.gz
 #reads=$wkdir/new_flowcell_calls/Ctenella_sup_no_symbionts.fastq.gz
-reads=/share/deepseq/laura/ctenella/kraken2/Ctenella_sup_k2_Montipora.fastq
+#reads=/share/deepseq/laura/ctenella/kraken2/Ctenella_sup_k2_Montipora.fastq
+reads=/share/deepseq/laura/ctenella/kraken2/Ctenella_sup_k2_Scleratinia.fastq.gz
 
 # print a line to the slurm output that says exactly what was done on this run
 echo "This is hifiasm version 0.25.0 running on the file $reads and saving the output to the directory $wkdir/hifiasm_asm$attempt"
@@ -61,11 +64,13 @@ cd $wkdir/hifiasm_asm$attempt
 
 # run hifiasm on the simplex reads with the new --ont flag to generate the assembly
 hifiasm \
---telo-m AACCCT \
--t 42 \
+-t 94 \
 --ont \
 -o ONTasm \
 $reads
+
+
+#--telo-m AACCCT \
 
 # convert the final assembly to fasta format
 awk '/^S/{print ">"$2;print $3}' ONTasm.bp.p_ctg.gfa > ONTasm.bp.p_ctg.fasta
