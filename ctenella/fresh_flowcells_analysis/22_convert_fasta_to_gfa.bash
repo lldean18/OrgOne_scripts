d2941684c423fe62ba8184db417b1870d2c06dde
Laura Dean <lldean18@gmail.com>
2026-02-28 13:09:33 +0000

    adding code to convert fasta to gfa

---
+++ ctenella/fresh_flowcells_analysis/22_convert_fasta_to_gfa.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Laura Dean
+# 28/2/26
+
+# code to convert fasta format to GFA to view in bandage
+
+source $HOME/.bash_profile
+conda create --name gfastats bioconda::gfastats
+conda activate gfastats
+
+cd /gpfs01/home/mbzlld/data/ctenella/hifiasm_asm4
+
+# convert fasta file to gfa format
+gfastats ONTasm.bp.p_ctg_Scleractinia_38-41GC_180-300X_100kb.fasta > ONTasm.bp.p_ctg_Scleractinia_38-41GC_180-300X_100kb.gfa
+
+conda deactivate
+
