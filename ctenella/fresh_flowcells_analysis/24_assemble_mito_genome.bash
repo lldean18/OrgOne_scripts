1020fdfe7ebfbb24215721dc07fcb951bcfb8633
Laura Dean <lldean18@gmail.com>
2026-03-11 17:05:17 +0000

    trying again to assembly the mitochondria

--- ctenella/fresh_flowcells_analysis/24_assemble_mito_genome.bash
+++ ctenella/fresh_flowcells_analysis/24_assemble_mito_genome.bash
@@ -9,9 +9,9 @@
 #SBATCH --partition=defq
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=96
-#SBATCH --mem=361g
-#SBATCH --time=168:00:00
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=100g
+#SBATCH --time=48:00:00
 #SBATCH --job-name=assemble_ctenella_mitochondria
 #SBATCH --output=/gpfs01/home/mbzlld/code_and_scripts/slurm_out_scripts/slurm-%x-%j.out
 
@@ -24,12 +24,25 @@ conda activate flye
 # updated flye to the latest version for this v2.9.6
 cd /gpfs01/home/mbzlld/data/ctenella/mitogenome
 
+###  # first try I ran it with:
+###  flye \
+###  --threads 96 \
+###  --out-dir flye_mito_asm \
+###  --nano-raw mito_reads.fastq.gz \
+###  --genome-size 20k
 
+
+# second try I ran it with:
 flye \
---threads 96 \
---out-dir flye_mito_asm \
---nano-raw mito_reads.fastq.gz \
---genome-size 20k
+--threads 8 \
+--out-dir flye_mito_asm_2 \
+--nano-raw mito_reads_miniprot.fastq.gz \
+--genome-size 18k
+
+
+
+
+
 
 #--nano-hq
 
