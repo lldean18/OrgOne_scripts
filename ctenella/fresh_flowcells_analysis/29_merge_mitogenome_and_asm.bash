08271c27781ef39a6131f1b8a07db86701059dfb
Laura Dean <lldean18@gmail.com>
2026-03-30 14:39:15 +0100

    indexing the ref

--- ctenella/fresh_flowcells_analysis/29_merge_mitogenome_and_asm.bash
+++ ctenella/fresh_flowcells_analysis/29_merge_mitogenome_and_asm.bash
@@ -5,3 +5,9 @@
 
 cat /gpfs01/home/mbzlld/data/ctenella/mitogenome/flye_mito_asm_2/assembly.fasta /gpfs01/home/mbzlld/data/ctenella/hifiasm_asm4/ONTasm.bp.p_ctg_Scleractinia_38-41GC_180-300X_100kb.fasta > /gpfs01/home/mbzlld/data/ctenella/ctenella_chagius_asm.fasta
 
+conda activate samtools1.22
+samtools faidx /gpfs01/home/mbzlld/data/ctenella/ctenella_chagius_asm.fasta
+conda deactivate
+
+
+
