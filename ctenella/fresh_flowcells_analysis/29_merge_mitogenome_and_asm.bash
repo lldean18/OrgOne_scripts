f70dca089e8f721e424328a3b838c07526a225be
Laura Dean <lldean18@gmail.com>
2026-03-30 13:18:48 +0100

    adding final assembly concatenation

---
+++ ctenella/fresh_flowcells_analysis/29_merge_mitogenome_and_asm.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+# 30/3/26
+
+# code to merge the final assembly with mitogenome for mapping and pop gen analysis
+
+cat /gpfs01/home/mbzlld/data/ctenella/hifiasm_asm4/ONTasm.bp.p_ctg_Scleractinia_38-41GC_180-300X_100kb.fasta /gpfs01/home/mbzlld/data/ctenella/mitogenome/flye_mito_asm_2/assembly.fasta > /gpfs01/home/mbzlld/data/ctenella/ctenella_chagius_asm.fasta
+
