158184e772fe42ce62364bf70f654f30934d51ca
Laura Dean <lldean18@gmail.com>
2026-04-01 10:42:09 +0100

    read filtering script as an array

--- ctenella/the_twelve/assembly/02_size_filter_reads.bash
+++ ctenella/the_twelve/assembly/02_size_filter_reads.bash
@@ -3,16 +3,29 @@
 
 # script to filter the raw ctenella reads to try an assembly with only the larger fragments
 
+#SBATCH --job-name=SizeFilterRawReads
+#SBATCH --partition=defq
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=12g
+#SBATCH --time=6:00:00
+#SBATCH --output=/gpfs01/home/mbzlld/code_and_scripts/slurm_out_scripts/slurm-%x-%j.out
+#SBATCH --array=1-12
+
+
 # setup env
 source $HOME/.bash_profile
 conda activate seqtk
 cd /gpfs01/home/mbzlld/data/ctenella/the_twelve/fastqs
 
+CONFIG=~/code_and_scripts/config_files/ctenella_the_twelve_config.txt
+ind=$(awk -v ArrayTaskID=$SLURM_ARRAY_TASK_ID '$1==ArrayTaskID {print $2}' $CONFIG)
+echo "slurm array = $SLURM_ARRAY_TASK_ID size filtering raw reads for sample $ind"
 
-# loop over fastqs and filter reads to >3.5kb or longer
-for f in *.fastq.gz; do
-    seqtk seq -L 3500 $f | gzip > ${f%.fastq.gz}_3.5kb.fastq.gz
-done
+
+# filter reads to >3.5kb or longer
+seqtk seq -L 3500 barcode${ind}.fastq.gz | gzip > barcode${ind}_3.5kb.fastq.gz
 
 
 # unload the software
