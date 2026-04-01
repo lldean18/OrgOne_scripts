334583f861c914040596b57a3e3f5106a7fa5241
Laura Dean <lldean18@gmail.com>
2026-04-01 11:02:07 +0100

    reducing time limit as it finshed super fast

--- ctenella/the_twelve/05_merge_variants.bash
+++ ctenella/the_twelve/05_merge_variants.bash
@@ -9,7 +9,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=96
 #SBATCH --mem=361g
-#SBATCH --time=80:00:00
+#SBATCH --time=5:00:00
 #SBATCH --output=/gpfs01/home/mbzlld/code_and_scripts/slurm_out_scripts/slurm-%x-%j.out
 
 
