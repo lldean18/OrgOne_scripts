9e8bd290f64f3b4bcef014fd6549eddc2c1ec0d5
Laura Dean <lldean18@gmail.com>
2026-04-01 13:46:22 +0100

    updating vcf filtering script

---
+++ ctenella/the_twelve/06_filter_variants.bash
@@ -0,0 +1,64 @@
+#!/bin/bash
+# 1/4/26
+
+# script to filter merged variants for the 12 ctenella samples
+
+#SBATCH --job-name=filter_variants
+#SBATCH --partition=defq
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=10g
+#SBATCH --time=5:00:00
+#SBATCH --output=/gpfs01/home/mbzlld/code_and_scripts/slurm_out_scripts/slurm-%x-%j.out
+
+# setup env
+source $HOME/.bash_profile
+module load bcftools-uoneasy/1.19-GCC-13.2.0
+module load vcftools-uoneasy/0.1.16-GCC-12.3.0
+module load plink-uoneasy/2.00a3.7-foss-2023a
+cd /gpfs01/home/mbzlld/data/ctenella/the_twelve/variants
+
+
+###  # filter for quality and depth
+###  bcftools filter \
+###    -e 'QUAL<30 || DP<10' \
+###    the_twelve.bcf \
+###    -Oz \
+###    --threads 16 \
+###    -o the_twelve_filtered.vcf.gz
+
+# filter to retain only biallelic snps
+bcftools view \
+  -v snps \
+  -m2 -M2 \
+  the_twelve_filtered.vcf.gz \
+  -Oz \
+  -o the_twelve_snps.vcf.gz
+
+# filter for missingness and minor allele frequency
+vcftools \
+  --gzvcf the_twelve_snps.vcf.gz \
+  --max-missing 0.9 \
+  --maf 0.05 \
+  --recode --stdout | bgzip > the_twelve_snps_0.9missing_0.5maf.vcf.gz
+
+# calculate LD with plink to make a list of sites to remove
+plink2 \
+  --vcf the_twelve_snps_0.9missing_0.5maf.vcf.gz \
+  --indep-pairwise 50 10 0.2 \
+  --out prune
+
+# remove snps in LD
+vcftools \
+  --gzvcf the_twelve_snps_0.9missing_0.5maf.vcf.gz \
+  --exclude-positions prune.prune.out \
+  --recode --stdout | bgzip > the_twelve_snps_0.9missing_0.5maf_LD0.2.vcf.gz
+
+
+
+
+module unload bcftools-uoneasy/1.19-GCC-13.2.0
+module unload vcftools-uoneasy/0.1.16-GCC-12.3.0
+module unload plink-uoneasy/2.00a3.7-foss-2023a
+
