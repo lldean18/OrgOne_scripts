846c6c28780de95b90a60e6dd90f7674861514bb
Laura Dean <lldean18@gmail.com>
2026-04-02 17:42:32 +0100

    adding script to generate stats about genotypes

---
+++ ctenella/the_twelve/07_count_variants_per_ind.bash
@@ -0,0 +1,27 @@
+#!/bin/bash
+# 1/4/26
+
+# script to generate stats abount genotypes in the 12 ctenella samples
+
+#SBATCH --job-name=filter_variants
+#SBATCH --partition=defq
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=10g
+#SBATCH --time=3:00:00
+#SBATCH --output=/gpfs01/home/mbzlld/code_and_scripts/slurm_out_scripts/slurm-%x-%j.out
+
+# setup env
+source $HOME/.bash_profile
+module load bcftools-uoneasy/1.19-GCC-13.2.0
+cd /gpfs01/home/mbzlld/data/ctenella/the_twelve/variants
+
+# generate info about individuals in vcf
+bcftools stats \
+--threads 16 \
+the_twelve_filtered.vcf.gz > the_twelve_filtered_stats.txt
+
+
+module unload bcftools-uoneasy/1.19-GCC-13.2.0
+
