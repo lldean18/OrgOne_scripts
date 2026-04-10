9af709afbe8bc90a434b0239701dc8e29726c053
Laura Dean <lldean18@gmail.com>
2026-04-10 12:32:10 +0100

    adding script to polish the draft assembly with dorado polish

---
+++ ctenella/fresh_flowcells_analysis/31_polish_assembly.bash
@@ -0,0 +1,36 @@
+#!/bin/bash
+# 10/4/26
+
+# script to polish the ctenella assembly with dorado polish
+
+#SBATCH --job-name=dorado_polish
+#SBATCH --partition=defq
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=48
+#SBATCH --mem=150g
+#SBATCH --time=100:00:00
+#SBATCH --output=/gpfs01/home/mbzlld/code_and_scripts/slurm_out_scripts/slurm-%x-%j.out
+
+
+# setup env
+source $HOME/.bash_profile
+conda activate samtools1.22
+assembly=/gpfs01/home/mbzlld/data/ctenella/hifiasm_asm4/ONTasm.bp.p_ctg_Scleractinia_38-41GC_180-300X_100kb.fasta
+
+
+# Align reads to a reference using dorado aligner, sort and index
+dorado aligner $assembly /share/deepseq/matt/Ctenella/Ctenella_sup.fastq.gz |
+samtools sort --threads 48 > ${assembly%.*}_mapped_reads.bam
+samtools index ${assembly%.*}_mapped_reads.bam
+
+
+# polish the draft assembly
+dorado polish \
+--threads 48 \
+${assembly%.*}_mapped_reads.bam \
+$assembly > ${assembly%.*}_polished.fasta
+
+
+conda deactivate
+
