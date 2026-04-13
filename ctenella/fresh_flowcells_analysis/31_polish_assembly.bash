f47ffa9737f07f9c3d4531106aa79320a5728904
Laura Dean <lldean18@gmail.com>
2026-04-13 15:55:29 +0100

    adding a polishing step for each of the two read groups of data we have in the bam file

--- ctenella/fresh_flowcells_analysis/31_polish_assembly.bash
+++ ctenella/fresh_flowcells_analysis/31_polish_assembly.bash
@@ -19,17 +19,25 @@ conda activate samtools1.22
 assembly=/gpfs01/home/mbzlld/data/ctenella/hifiasm_asm4/ONTasm.bp.p_ctg_Scleractinia_38-41GC_180-300X_100kb.fasta
 
 
-# Align reads to a reference using dorado aligner, sort and index
-dorado aligner $assembly /share/deepseq/matt/Ctenella/Ctenella.sup.meth.bam |
-samtools sort --threads 48 > ${assembly%.*}_mapped_reads.bam
-samtools index ${assembly%.*}_mapped_reads.bam
+###  # Align reads to a reference using dorado aligner, sort and index
+###  dorado aligner $assembly /share/deepseq/matt/Ctenella/Ctenella.sup.meth.bam |
+###  samtools sort --threads 48 > ${assembly%.*}_mapped_reads.bam
+###  samtools index ${assembly%.*}_mapped_reads.bam
 
 
 # polish the draft assembly
 dorado polish \
 --threads 48 \
+--RG 1a4a3b36-aa31-420d-acde-f8d38087de7a_dna_r10.4.1_e8.2_400bps_sup@v5.2.0 \
 ${assembly%.*}_mapped_reads.bam \
-$assembly > ${assembly%.*}_polished.fasta
+$assembly > ${assembly%.*}_polished_1.fasta
+
+# polish the draft assembly again with the 2nd read group
+dorado polish \
+--threads 48 \
+--RG b90d191e-d354-4888-a386-83de53d6f4c7_dna_r10.4.1_e8.2_400bps_sup@v5.2.0 \
+${assembly%.*}_mapped_reads.bam \
+${assembly%.*}_polished_1.fasta > ${assembly%.*}_polished_final.fasta
 
 
 conda deactivate
