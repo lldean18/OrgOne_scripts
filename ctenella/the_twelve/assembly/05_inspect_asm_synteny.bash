5eb2b9ec69d7b436ec3b6bb501e4a91acdaec65e
Laura Dean <lldean18@gmail.com>
2026-04-16 10:45:34 +0100

    first attempt at plotting synteny between the ctenella assemblies

--- ctenella/the_twelve/assembly/05_inspect_asm_synteny.bash
+++ ctenella/the_twelve/assembly/05_inspect_asm_synteny.bash
@@ -7,8 +7,8 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=20g
-#SBATCH --time=2:00:00
+#SBATCH --mem=50g
+#SBATCH --time=48:00:00
 #SBATCH --job-name=ctenella_synteny
 #SBATCH --output=/gpfs01/home/mbzlld/code_and_scripts/slurm_out_scripts/slurm-%x-%j.out
 
@@ -16,19 +16,31 @@
 source $HOME/.bash_profile
 #conda create --name ntsynt -c bioconda -c conda-forge ntsynt
 conda activate ntsynt
-cd /gpfs01/home/mbzlld/data/ctenella/the_twelve/assemblies
+mkdir -p /gpfs01/home/mbzlld/data/ctenella/the_twelve/assemblies/ntsynt
+cd /gpfs01/home/mbzlld/data/ctenella/the_twelve/assemblies/ntsynt 
 
+###  # make a list of the assemblies to plot
+###  echo "/gpfs01/home/mbzlld/data/ctenella/hifiasm_asm4/ONTasm.bp.p_ctg_Scleractinia_38-41GC_180-300X_100kb.fasta" > assemblies_list.txt
+###  ls /gpfs01/home/mbzlld/data/ctenella/the_twelve/assemblies/barcode*/barcode*_ONTasm.bp.p_ctg.fasta >> assemblies_list.txt
+###  sed -i 's|^[^/]*/|/|' assemblies_list.txt 
 
-# make a list of the assemblies to plot
-#ls barcode*/barcode*_ONTasm.bp.p_ctg.fasta > assemblies_list.txt
-
-# estimate divergence between sequences
-
-
-# plot synteny between assemblies
+# calculate synteny between assemblies
 ntSynt \
   --fastas_list assemblies_list.txt \
-  --divergence
+  --divergence 0.5
+
+# make a list of the assembly .fai files
+ls *.fai > fai_list.txt
+
+# plot synteny
+ntsynt_viz.py \
+--normalize \
+--blocks ntSynt.k24.w1000.synteny_blocks.tsv \
+--fais fai_list.txt \
+--prefix ribbon_plot \
+--target-genome ONTasm.bp.p_ctg_Scleractinia_38-41GC_180-300X_100kb.fasta \
+--no-arrow \
+--ribbon_adjust 2.5
 
 # unload software
 conda deactivate
