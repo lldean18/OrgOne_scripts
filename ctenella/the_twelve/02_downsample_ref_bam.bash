2d586a4d6a5de7d167b30de8afb40b19d9054f2b
Laura Dean <lldean18@gmail.com>
2026-04-20 14:42:28 +0100

    getting the syntax right for rasusa

--- ctenella/the_twelve/02_downsample_ref_bam.bash
+++ ctenella/the_twelve/02_downsample_ref_bam.bash
@@ -17,33 +17,42 @@ source $HOME/.bash_profile
 conda activate minimap2
 cd /gpfs01/home/mbzlld/data/ctenella/new_flowcell_calls
 
-# start by mapping the raw ctenella ref reads to the polished reference
-minimap2 \
--ax map-ont \
--t 16 \
-/gpfs01/home/mbzlld/data/ctenella/hifiasm_asm4/ONTasm.bp.p_ctg_Scleractinia_38-41GC_180-300X_100kb_polished_1.fasta \
-/gpfs01/home/mbzlld/data/ctenella/new_flowcell_calls/Ctenella_sup_3.5kb.fastq |
-samtools sort --threads 16 -o Ctenella_sup_3.5kb.bam
-samtools index --threads 16 Ctenella_sup_3.5kb.bam
+#  # start by mapping the raw ctenella ref reads to the polished reference
+#  minimap2 \
+#  -ax map-ont \
+#  -t 16 \
+#  /gpfs01/home/mbzlld/data/ctenella/hifiasm_asm4/ONTasm.bp.p_ctg_Scleractinia_38-41GC_180-300X_100kb_polished_1.fasta \
+#  /gpfs01/home/mbzlld/data/ctenella/new_flowcell_calls/Ctenella_sup_3.5kb.fastq |
+#  samtools sort --threads 16 -o Ctenella_sup_3.5kb.bam
+#  samtools index --threads 16 Ctenella_sup_3.5kb.bam
+#  
+#  # filter to retain only good mappings
+#  samtools view \
+#  -b \
+#  -q 30 \
+#  -F 260 \
+#  Ctenella_sup_3.5kb.bam |
+#  samtools sort --threads 16 -o Ctenella_sup_3.5kb_flt.bam
+#  samtools index --threads 16 Ctenella_sup_3.5kb_flt.bam
+#  
+#  # compute the current coverage of the reference bam
+#  samtools depth \
+#  Ctenella_sup_3.5kb_flt.bam |
+#  awk '{sum+=$3} END {print sum/NR}' > Ctenella_sup_3.5kb_flt.bam.depth
 
-# filter to retain only good mappings
-samtools view \
--b \
--q 30 \
--F 260 \
-Ctenella_sup_3.5kb.bam |
-samtools sort --threads 16 -o Ctenella_sup_3.5kb_flt.bam
-samtools index --threads 16 Ctenella_sup_3.5kb_flt.bam
-
-# compute the current coverage of the reference bam
-samtools depth \
+# randomly subsample to the desired coverage
+rasusa aln \
+--coverage 22 \
 Ctenella_sup_3.5kb_flt.bam |
-awk '{sum+=$3} END {print sum/NR}' > Ctenella_sup_3.5kb_flt.bam.depth
-
-# calculate sampling fraction (target divided by current)
-#23 / 
-
+samtools sort --threads 16 -o /gpfs01/home/mbzlld/data/ctenella/the_twelve/bams/remap_sort_barcoderef.bam
+samtools index --threads 16 /gpfs01/home/mbzlld/data/ctenella/the_twelve/bams/remap_sort_barcoderef.bam
 # save the final ind same as others but barcode ref
+
+# check final depth
+echo "the final depth of the downsampled bam file is:"
+samtools depth \
+/gpfs01/home/mbzlld/data/ctenella/the_twelve/bams/remap_sort_barcoderef.bam \
+| awk '{sum+=$3} END {print sum/NR}'
 
 
 conda deactivate
