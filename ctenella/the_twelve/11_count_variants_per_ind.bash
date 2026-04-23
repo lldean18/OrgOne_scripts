d52fadccea391446cfb859ea3b79ce017d414a78
Laura Dean <lldean18@gmail.com>
2026-04-23 15:40:31 +0100

    updating to write counts to a file

--- ctenella/the_twelve/11_count_variants_per_ind.bash
+++ ctenella/the_twelve/11_count_variants_per_ind.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
-# 1/4/26
+# 23/4/26
 
-# script to generate stats abount genotypes in the 12 ctenella samples
+# script to generate stats abount genotypes in the 13 ctenella samples
 
 #SBATCH --job-name=variant_stats
 #SBATCH --partition=defq
@@ -17,89 +17,40 @@ source $HOME/.bash_profile
 module load bcftools-uoneasy/1.19-GCC-13.2.0
 cd /gpfs01/home/mbzlld/data/ctenella/the_twelve/variants
 
-###  # generate info about individuals in vcf
-###  bcftools stats \
-###  --threads 16 \
-###  the_twelve_filtered.vcf.gz > the_twelve_filtered_stats.txt
-# this wasnt really what I was after, try a different option
-
-
-bcftools query -f '[%SAMPLE\t%GT\n]' the_twelve_filtered.vcf.gz \
-| awk '$2!="0/0" && $2!="./." && $2!~/\./' \
-| cut -f1 \
-| sort \
-| uniq -c
 
 # count the number of variants that differ from the reference
-bcftools query -f '[%SAMPLE\t%GT\n]' the_twelve_snps_0.1maf_nopartialcalls_nomissing.vcf.gz \
-| awk '$2!="0/0" && $2!="./." && $2!~/\./' \
-| cut -f1 \
-| sort \
-| uniq -c
+# for all files in a list and write to an output file
+rm non_ref_variant_counts.tsv
+files=(the_thirteen_Q30_DP10_SNP_mis1_maf0.1.vcf.gz the_thirteen_Q30_DP15_SNP_mis1_maf0.1.vcf.gz the_thirteen_Q30_DP20_SNP_mis1_maf0.1.vcf.gz the_thirteen_Q30_DP10_SNP_mis1_maf0.05.vcf.gz the_thirteen_Q30_DP15_SNP_mis1_maf0.05.vcf.gz the_thirteen_Q30_DP20_SNP_mis1_maf0.05.vcf.gz)
+for file in ${files[@]}
+do
+bcftools query -f '[%SAMPLE\t%GT\n]' $file \
+| awk '$2!="0/0" && $2!="./." && $2!~/\./' | cut -f1 | sort | uniq -c > tmp
+sed -i "s/$/\t${file%.vcf.gz}/" tmp
+cat tmp >> non_ref_variant_counts.tsv
+rm tmp
+done
 
-### 0.05 MAF
-#  212657 barcode17
-#  232759 barcode18
-#  233928 barcode19
-#  230534 barcode20
-#  223021 barcode21
-#  219241 barcode22
-#  235436 barcode23
-#  209501 barcode24
-#  213517 barcode29
-#  144702 barcode30
-#  213839 barcode31
-#  217648 barcode32
+# make the output file fully tab separated
+awk '{print $1, $2, $3}' OFS='\t' non_ref_variant_counts.tsv > tmp && mv tmp non_ref_variant_counts.tsv
 
-### 0.1 MAF
-# 170554 barcode17
-# 185800 barcode18
-# 190103 barcode19
-# 184025 barcode20
-# 179826 barcode21
-# 176407 barcode22
-# 188537 barcode23
-# 166636 barcode24
-# 171340 barcode29
-# 115184 barcode30
-# 170363 barcode31
-# 175011 barcode32
-
+# count the number of variants that differ from the reference (for a single file at a time)
+bcftools query -f '[%SAMPLE\t%GT\n]' the_thirteen_Q30_DP15_SNP_mis1_maf0.1.vcf.gz \
+| awk '$2!="0/0" && $2!="./." && $2!~/\./' | cut -f1 | sort | uniq -c
 
 # count the number of sites that match the reference
-bcftools query -f '[%SAMPLE\t%GT\n]' the_twelve_snps_0.1maf_nopartialcalls_nomissing.vcf.gz \
-| awk '$2=="0/0"' \
-| cut -f1 \
-| sort \
-| uniq -c
+bcftools query -f '[%SAMPLE\t%GT\n]' the_thirteen_Q30_DP15_SNP_mis1_maf0.1.vcf.gz \
+| awk '$2=="0/0"' | cut -f1 | sort | uniq -c
 
-### 0.05 MAF
-#  355735 barcode17
-#  335633 barcode18
-#  334464 barcode19
-#  337858 barcode20
-#  345371 barcode21
-#  349151 barcode22
-#  332956 barcode23
-#  358891 barcode24
-#  354875 barcode29
-#  423690 barcode30
-#  354553 barcode31
-#  350744 barcode32
 
-### 0.1 MAF
-# 139197 barcode17
-# 123951 barcode18
-# 119648 barcode19
-# 125726 barcode20
-# 129925 barcode21
-# 133344 barcode22
-# 121214 barcode23
-# 143115 barcode24
-# 138411 barcode29
-# 194567 barcode30
-# 139388 barcode31
-# 134740 barcode32
+
+
+
+
+
+
+
+
 
 
 # count the number of SNPs per contig
