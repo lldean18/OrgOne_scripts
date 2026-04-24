0e7ee07e6628ebc41f366de7154e4cd3f2427a87
Laura Dean <lldean18@gmail.com>
2026-04-24 12:00:21 +0100

    trying with invariant sites removed

--- ctenella/the_twelve/14_RaxML-NG_phylogeny.bash
+++ ctenella/the_twelve/14_RaxML-NG_phylogeny.bash
@@ -7,7 +7,7 @@
 #SBATCH --partition=defq
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=8
+#SBATCH --cpus-per-task=16
 #SBATCH --mem=50g
 #SBATCH --time=24:00:00
 #SBATCH --output=/gpfs01/home/mbzlld/code_and_scripts/slurm_out_scripts/slurm-%x-%j.out
@@ -18,10 +18,10 @@ source $HOME/.bash_profile
 conda activate raxmlng
 mkdir -p /gpfs01/home/mbzlld/data/ctenella/the_twelve/raxmlng
 
-# # convert VCF to phylip format
-# cd /gpfs01/home/mbzlld/data/ctenella/the_twelve/variants
-# python ~/software_bin/vcf2phylip.py --input the_twelve_Q30_DP10_SNP_mis1_maf0.1.vcf.gz
-# mv the_twelve_Q30_DP10_SNP_mis1_maf0.1.min4.phy the_twelve_Q30_DP10_SNP_mis1_maf0.1.phy
+# convert VCF to phylip format
+cd /gpfs01/home/mbzlld/data/ctenella/the_twelve/variants
+python ~/software_bin/vcf2phylip.py --input the_twelve_Q30_DP10_SNP_mis1_maf0.1_onlyvar.vcf.gz
+mv the_twelve_Q30_DP10_SNP_mis1_maf0.1_onlyvar.min4.phy the_twelve_Q30_DP10_SNP_mis1_maf0.1_onlyvar.phy
 
 # # check raxml can read the input file correctly
 # raxml-ng --check --msa the_twelve_Q30_DP10_SNP_mis1_maf0.1.phy --model GTR+G --prefix R1
@@ -30,11 +30,11 @@ cd /gpfs01/home/mbzlld/data/ctenella/the_twelve/raxmlng
 
 # make tree
 raxml-ng \
-  --msa /gpfs01/home/mbzlld/data/ctenella/the_twelve/variants/the_twelve_Q30_DP10_SNP_mis1_maf0.1.phy \
-  --model GTR+G \
-  --seed 2 --threads 8 \
+  --msa /gpfs01/home/mbzlld/data/ctenella/the_twelve/variants/the_twelve_Q30_DP10_SNP_mis1_maf0.1_onlyvar.phy \
+  --model GTR+ASC_LEWIS \
+  --seed 2 --threads 16 \
   --bootstrap --bs-trees 1000 \
-  --prefix Raxml_GTR-G_BS1000
+  --prefix Raxml_GTR-ASC_LEWIS_BS1000
 
 
 conda deactivate
