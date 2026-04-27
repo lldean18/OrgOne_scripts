8bdb03af374f9d3fc9b5639c050e74aba42dd77e
Laura Dean <lldean18@gmail.com>
2026-04-27 15:19:35 +0100

    updating to run for the hap1 assembly

--- ctenella/fresh_flowcells_analysis/19_merge_contig_stats.bash
+++ ctenella/fresh_flowcells_analysis/19_merge_contig_stats.bash
@@ -8,11 +8,11 @@ cd /gpfs01/home/mbzlld/data/ctenella/hifiasm_asm4
 
 # clean up and sort the 3 files of data
 # coverage
-awk '{print $1, $2}' ONTasm.bp.p_ctg_mapped_raw_reads_Scleractinia_depth.txt | sort -k1,1 > cov.txt
+awk '{print $1, $2}' ONTasm.bp.hap1.p_ctg_mapped_raw_reads_Scleractinia_depth.txt | sort -k1,1 > cov.txt
 # length (extract taxid)
-awk '{split($2,a,"|"); print $1, a[2], $3}' ONTasm.bp.p_ctg_Scleractinia.fasta_length.tsv | sort -k1,1 > len.txt
+awk '{split($2,a,"|"); print $1, a[2], $3}' ONTasm.bp.hap1.p_ctg_Scleractinia.fasta_length.tsv | sort -k1,1 > len.txt
 # GC (extract taxid)
-awk '{split($2,a,"|"); print $1, a[2], $3}' ONTasm.bp.p_ctg_Scleractinia.fasta_GC.tsv | sort -k1,1 > gc.txt
+awk '{split($2,a,"|"); print $1, a[2], $3}' ONTasm.bp.hap1.p_ctg_Scleractinia.fasta_GC.tsv | sort -k1,1 > gc.txt
 
 
 # join the 3 separate datasets
@@ -21,7 +21,7 @@ join tmp.txt gc.txt > merged.txt
 
 # add column headings and remove the duplicate taxid column
 awk 'BEGIN{print "contig\tcoverage\ttaxid\tlength\tGC"}
-     {print $1, $2, $3, $4, $6}' OFS="\t" merged.txt > final_table.tsv
+     {print $1, $2, $3, $4, $6}' OFS="\t" merged.txt > final_table_hap1.tsv
 
 
 rm cov.txt len.txt gc.txt tmp.txt merged.txt
