58537f1ec4270239408da17de6065b385a9a2ae3
Laura Dean <lldean18@gmail.com>
2026-04-28 11:36:43 +0100

    changing the filtering for the hap1 asm a bit based on the plotly plot in R

--- ctenella/fresh_flowcells_analysis/21_filter_assembly.bash
+++ ctenella/fresh_flowcells_analysis/21_filter_assembly.bash
@@ -25,34 +25,34 @@ asm=ONTasm.bp.hap1.p_ctg_Scleractinia.fasta
 
 ########### FILTER GC
 # Make a list of contig IDs with GC content between 38 and 41
-awk -F'\t' '$2 >= 38 && $2 <= 41' ${asm}_GC.tsv > Contig_IDs_38-41GC_hap1.tsv
-sed -i 's/\t.*//' Contig_IDs_38-41GC_hap1.tsv
+awk -F'\t' '$2 >= 39 && $2 <= 40.5' ${asm}_GC.tsv > Contig_IDs_39-40.5GC_hap1.tsv
+sed -i 's/\t.*//' Contig_IDs_39-40.5GC_hap1.tsv
 
 # filter the reads to contain only those in the list of read IDs
 seqkit grep \
 -n \
--f Contig_IDs_38-41GC_hap1.tsv \
-$asm > ${asm%.*}_38-41GC.fasta
+-f Contig_IDs_39-40.5GC_hap1.tsv \
+$asm > ${asm%.*}_39-40.5GC.fasta
 
 
 ############ FILTER DEPTH
 # make a list of contig coverage depth between 180 and 300 X
-awk -F' ' '$2 >= 180 && $2 <= 300' ONTasm.bp.hap1.p_ctg_mapped_raw_reads_Scleractinia_depth.txt > Contig_IDs_180-300X_hap1.txt
-sed -i 's/ .*//' Contig_IDs_180-300X_hap1.txt
+awk -F' ' '$2 >= 220 && $2 <= 300' ONTasm.bp.hap1.p_ctg_mapped_raw_reads_Scleractinia_depth.txt > Contig_IDs_220-300X_hap1.txt
+sed -i 's/ .*//' Contig_IDs_220-300X_hap1.txt
 
 # filter the reads to contain only those in the list of read IDs
 seqkit grep \
--f Contig_IDs_180-300X_hap1.txt \
-${asm%.*}_38-41GC.fasta > ${asm%.*}_38-41GC_180-300X.fasta
+-f Contig_IDs_220-300X_hap1.txt \
+${asm%.*}_39-40.5GC.fasta > ${asm%.*}_39-40.5GC_220-300X.fasta
 
 
 ############ FILTER CONTIG LENGTH
 seqtk seq \
--L 100000 ${asm%.*}_38-41GC_180-300X.fasta > ${asm%.*}_38-41GC_180-300X_100kb.fasta
+-L 100000 ${asm%.*}_39-40.5GC_220-300X.fasta > ${asm%.*}_39-40.5GC_220-300X_100kb.fasta
 
 
 ######### EXTRACT FINAL CONTIG NAMES FOR R FILTERING
-grep "^>" ${asm%.*}_38-41GC_180-300X_100kb.fasta | sed 's/^>//' > ${asm%.*}_38-41GC_180-300X_100kb_contig_names.txt
+grep "^>" ${asm%.*}_39-40.5GC_220-300X_100kb.fasta | sed 's/^>//' > ${asm%.*}_39-40.5GC_220-300X_100kb_contig_names.txt
 
 
 conda deactivate
