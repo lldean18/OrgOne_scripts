10ec700bf941aecc4997f92ee988e6e783ce8e6e
Laura Dean <lldean18@gmail.com>
2026-04-28 11:00:25 +0100

    script for making phylogeny

--- ctenella/the_twelve/14_RaxML-NG_phylogeny.bash
+++ ctenella/the_twelve/14_RaxML-NG_phylogeny.bash
@@ -18,23 +18,48 @@ source $HOME/.bash_profile
 conda activate raxmlng
 mkdir -p /gpfs01/home/mbzlld/data/ctenella/the_twelve/raxmlng
 
-# convert VCF to phylip format
-cd /gpfs01/home/mbzlld/data/ctenella/the_twelve/variants
-python ~/software_bin/vcf2phylip.py --input the_twelve_Q30_DP10_SNP_mis1_maf0.1_onlyvar.vcf.gz
-mv the_twelve_Q30_DP10_SNP_mis1_maf0.1_onlyvar.min4.phy the_twelve_Q30_DP10_SNP_mis1_maf0.1_onlyvar.phy
+# # convert the twelve VCF to phylip format
+# cd /gpfs01/home/mbzlld/data/ctenella/the_twelve/variants
+# python ~/software_bin/vcf2phylip.py --fasta --input the_twelve_Q30_DP10_SNP_mis1_maf0.1.vcf.gz
+# mv the_twelve_Q30_DP10_SNP_mis1_maf0.1.min4.phy the_twelve_Q30_DP10_SNP_mis1_maf0.1.phy
+# mv the_twelve_Q30_DP10_SNP_mis1_maf0.1.min4.fasta the_twelve_Q30_DP10_SNP_mis1_maf0.1.fasta
+
+# # convert the thirteen VCF to phylip format
+# cd /gpfs01/home/mbzlld/data/ctenella/the_twelve/variants
+# python ~/software_bin/vcf2phylip.py --input the_thirteen_Q30_DP10_SNP_mis1_maf0.1.vcf.gz
+# mv the_thirteen_Q30_DP10_SNP_mis1_maf0.1.min4.phy the_thirteen_Q30_DP10_SNP_mis1_maf0.1.phy
+
+# # remove invariant sites from the alignment
+# snp-sites -p /gpfs01/home/mbzlld/data/ctenella/the_twelve/variants/the_twelve_Q30_DP10_SNP_mis1_maf0.1.fasta \
+# > /gpfs01/home/mbzlld/data/ctenella/the_twelve/variants/the_twelve_Q30_DP10_SNP_mis1_maf0.1_onlyvar.phy
 
 # # check raxml can read the input file correctly
 # raxml-ng --check --msa the_twelve_Q30_DP10_SNP_mis1_maf0.1.phy --model GTR+G --prefix R1
 
 cd /gpfs01/home/mbzlld/data/ctenella/the_twelve/raxmlng
 
-# make tree
+# make bootstrap trees
 raxml-ng \
-  --msa /gpfs01/home/mbzlld/data/ctenella/the_twelve/variants/the_twelve_Q30_DP10_SNP_mis1_maf0.1_onlyvar.phy \
-  --model GTR+ASC_LEWIS \
+  --msa /gpfs01/home/mbzlld/data/ctenella/the_twelve/variants/the_thirteen_Q30_DP10_SNP_mis1_maf0.1.phy \
+  --model GTR+G \
   --seed 2 --threads 16 \
   --bootstrap --bs-trees 1000 \
-  --prefix Raxml_GTR-ASC_LEWIS_BS1000
+  --prefix Raxml_thirteen_GTR-G_BS1000
+
+# make a majority rule consensus tree from the bootstrap replicates
+raxml-ng --consense --tree Raxml_thirteen_GTR-G_BS1000.raxml.bootstraps
+
+
+# # make tree and bootstrap in same step
+# raxml-ng \
+#   --all \
+#   --msa /gpfs01/home/mbzlld/data/ctenella/the_twelve/variants/the_twelve_Q30_DP10_SNP_mis1_maf0.1_onlyvar.phy \
+#    --model GTR+G \ 
+#   --prefix Raxml_GTR-G_2 \
+#   --seed 2 --threads 16 \
+#   --bs-metric fbp,tbe
+
+
 
 
 conda deactivate
