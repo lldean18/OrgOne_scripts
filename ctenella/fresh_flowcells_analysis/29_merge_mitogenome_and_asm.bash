a0cdc3ec3c2d76e8a21cd05c8aa2193c156dcbf3
Laura Dean <lldean18@gmail.com>
2026-04-28 10:54:02 +0100

    remerging the polished asm

--- ctenella/fresh_flowcells_analysis/29_merge_mitogenome_and_asm.bash
+++ ctenella/fresh_flowcells_analysis/29_merge_mitogenome_and_asm.bash
@@ -3,11 +3,18 @@
 
 # code to merge the final assembly with mitogenome for mapping and pop gen analysis
 
-cat /gpfs01/home/mbzlld/data/ctenella/mitogenome/flye_mito_asm_2/assembly.fasta /gpfs01/home/mbzlld/data/ctenella/hifiasm_asm4/ONTasm.bp.p_ctg_Scleractinia_38-41GC_180-300X_100kb.fasta > /gpfs01/home/mbzlld/data/ctenella/ctenella_chagius_asm.fasta
+# this one was prior to polishing
+cat /gpfs01/home/mbzlld/data/ctenella/mitogenome/flye_mito_asm_2/assembly.fasta /gpfs01/home/mbzlld/data/ctenella/hifiasm_asm4/ONTasm.bp.p_ctg_Scleractinia_38-41GC_180-300X_100kb.fasta > /gpfs01/home/mbzlld/data/ctenella/ctenella_chagius_asm_unpolished.fasta
+
+conda activate samtools1.22
+samtools faidx /gpfs01/home/mbzlld/data/ctenella/ctenella_chagius_asm_unpolished.fasta
+conda deactivate
+
+# this one is after polishing both asms
+cat /gpfs01/home/mbzlld/data/ctenella/mitogenome/flye_mito_asm_2/assembly_polished_final.fasta /gpfs01/home/mbzlld/data/ctenella/hifiasm_asm4/ONTasm.bp.p_ctg_Scleractinia_38-41GC_180-300X_100kb_polished_1.fasta > /gpfs01/home/mbzlld/data/ctenella/ctenella_chagius_asm.fasta
 
 conda activate samtools1.22
 samtools faidx /gpfs01/home/mbzlld/data/ctenella/ctenella_chagius_asm.fasta
 conda deactivate
-
 
 
