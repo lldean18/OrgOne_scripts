12552b3c6500de136be9cb0bf7b6f3896f5d03a0
Laura Dean <lldean18@gmail.com>
2026-06-01 16:27:20 +0100

    updating contig stats info

--- snake_necked_turtle/11_merge_contig_stats.bash
+++ snake_necked_turtle/11_merge_contig_stats.bash
@@ -20,3 +20,17 @@ awk 'BEGIN{print "contig\tcoverage\tlength\tGC"}
 
 
 rm cov.txt tmp.txt merged.txt
+
+
+# from looking at the contig stats in R we should filter:
+# GC 39-47%
+# length 100kb plus
+# depth 10X to 100X
+
+
+# ptg000185l and ptg000195l# part of the wierd smaller cluster of hi GC content contigs these were predicted Malaclemys terrapin pileata rRNA
+# ptg000213l # suuuuuper high coverage same as above
+# ptg000210l # suuuuper low GC content - this was highly highly repetative sequence, quite possibly telomeric
+
+
+
