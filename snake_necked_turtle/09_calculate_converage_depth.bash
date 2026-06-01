9fb99a84031bc23943bf8d85d692d31309f24ea5
Laura Dean <lldean18@gmail.com>
2026-06-01 11:06:04 +0100

    adding untracked files from before holls

---
+++ snake_necked_turtle/09_calculate_converage_depth.bash
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Laura Dean
+# 14/5/26
+
+# Script to calculate coverage depth
+
+#SBATCH --job-name=depth_calc
+#SBATCH --partition=defq
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=20g
+#SBATCH --time=8:00:00
+#SBATCH --output=/gpfs01/home/mbzlld/code_and_scripts/slurm_out_scripts/slurm-%x-%j.out
+
+
+# set up env
+source $HOME/.bash_profile
+conda activate samtools1.22
+bam=/share/deepseq/org_one/SNT052/hifiasm/turtle.bp.p_ctg_mapped_reads.bam
+
+
+# calculate coverage depth for each contig
+samtools depth -a $bam |
+awk '{sum[$1]+=$3; count[$1]++} END {for (c in sum) print c, sum[c]/count[c]}' > ${bam%.*}_depth.txt
+
+
+conda deactivate
