2b14a8d9e514d3e4032bd219d9673bfef3713f40
Laura Dean <lldean18@gmail.com>
2026-06-08 11:49:47 +0100

    uploading more tiger basecalled bams

--- ENA_submission/03_read_submission_bams.bash
+++ ENA_submission/03_read_submission_bams.bash
@@ -13,11 +13,12 @@
 
 
 # set bam file for upload
-bam=/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/basecalls/sumatran_tiger_P1/20230111_1737_3D_PAM34692_e5400fa7/pod5/tig_P1_SUP_basecalls.bam
-
+#bam=/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/basecalls/sumatran_tiger_P1/20230111_1737_3D_PAM34692_e5400fa7/pod5/tig_P1_SUP_basecalls.bam
+#bam=/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/basecalls/sumatran_tiger_P2/20230113_1342_3F_PAM70827_70386f08/pod5/tig_P2_SUP_basecalls.bam
+bam=/gpfs01/home/mbzlld/data/OrgOne/sumatran_tiger/basecalls/sumatran_tiger_P3/20230119_1328_1G_PAM34749_b65364c2/pod5/tig_P3_SUP_basecalls.bam
 
 # submit the bam file to ENA
-curl --upload-file $bam --user Webin-154:******** ftp://webin2.ebi.ac.uk
+curl --upload-file $bam --user Webin-154:hjsH3ZTp ftp://webin2.ebi.ac.uk
 
 echo "finished submission for file $bam"
 
