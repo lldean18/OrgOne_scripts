aae0054644632638726b0695421135df3983819a
Laura Dean <lldean18@gmail.com>
2026-06-08 16:44:14 +0100

    updating to run on newest vcf

--- ctenella/the_twelve/12_estimate_relatedness.bash
+++ ctenella/the_twelve/12_estimate_relatedness.bash
@@ -5,18 +5,19 @@
 
 
 # setup env
-cd /gpfs01/home/mbzlld/data/ctenella/the_twelve/variants
 module load vcftools-uoneasy/0.1.16-GCC-12.3.0
+mkdir -p /gpfs01/home/mbzlld/data/ctenella/the_twelve/relatedness
+cd /gpfs01/home/mbzlld/data/ctenella/the_twelve/relatedness
 
 
 # estimate relatedness based on the method of Yang et al, Nature Genetics 2010 (doi:10.1038/ng.608)
 vcftools \
---gzvcf the_twelve_snps_0.5maf_nopartialcalls_nomissing.vcf.gz \
+--gzvcf /gpfs01/home/mbzlld/data/ctenella/the_twelve/variants/the_thirteen_Q30_DP10_SNP_mis1_maf0.1.vcf.gz \
 --relatedness
 
 # estimate relatedness based on the method of Manichaikul et al., BIOINFORMATICS 2010 (doi:10.1093/bioinformatics/btq559)
 vcftools \
---gzvcf the_twelve_snps_0.5maf_nopartialcalls_nomissing.vcf.gz \
+--gzvcf /gpfs01/home/mbzlld/data/ctenella/the_twelve/variants/the_thirteen_Q30_DP10_SNP_mis1_maf0.1.vcf.gz \
 --relatedness2
 
 
