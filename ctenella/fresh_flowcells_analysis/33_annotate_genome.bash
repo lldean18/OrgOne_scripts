68347ee0bd3ad339713c89ade9bea62b965abea3
Laura Dean <lldean18@gmail.com>
2026-06-09 17:22:26 +0100

    trying again with container

--- ctenella/fresh_flowcells_analysis/33_annotate_genome.bash
+++ ctenella/fresh_flowcells_analysis/33_annotate_genome.bash
@@ -7,18 +7,18 @@
 #SBATCH --partition=defq
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=16
-#SBATCH --mem=80g
-#SBATCH --time=100:00:00
+#SBATCH --cpus-per-task=17
+#SBATCH --mem=360g
+#SBATCH --time=168:00:00
 #SBATCH --output=/gpfs01/home/mbzlld/code_and_scripts/slurm_out_scripts/slurm-%x-%j.out
 
 # setup env
-source $HOME/.bash_profile
-cd /gpfs01/home/mbzlld/data/ctenella/hifiasm_asm4/annotation
+module load singularity/3.8.5
+cd /gpfs01/home/mbzlld/data/ctenella/braker
 
 ##  # download protein sequences from other corals to train the annotation
-##  mkdir -p /gpfs01/home/mbzlld/data/ctenella/hifiasm_asm4/annotation/training_proteins
-##  cd /gpfs01/home/mbzlld/data/ctenella/hifiasm_asm4/annotation/training_proteins
+##  mkdir -p /gpfs01/home/mbzlld/data/ctenella/braker/training_proteins
+##  cd /gpfs01/home/mbzlld/data/ctenella/braker/training_proteins
 ##  wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/013/753/865/GCF_013753865.1_Amil_v2.1/GCF_013753865.1_Amil_v2.1_protein.faa.gz
 ##  wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/222/465/GCF_000222465.1_Adig_1.1/GCF_000222465.1_Adig_1.1_protein.faa.gz
 ##  wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/002/571/385/GCF_002571385.2_Stylophora_pistillata_v1.1/GCF_002571385.2_Stylophora_pistillata_v1.1_protein.faa.gz
@@ -28,17 +28,19 @@ cd /gpfs01/home/mbzlld/data/ctenella/hifiasm_asm4/annotation
 ##  rm GCF_013753865.1_Amil_v2.1_protein.faa.gz GCF_932526225.1_jaNemVect1.1_protein.faa.gz GCF_000222465.1_Adig_1.1_protein.faa.gz GCF_002571385.2_Stylophora_pistillata_v1.1_protein.faa.gz
 
 # make gene predictions with braker3
-conda activate braker3
-export PATH=$CONDA_PREFIX/bin:$PATH
-export PERL5LIB=/gpfs01/home/mbzlld/software_bin/miniconda3/envs/braker3/lib/perl5/site_perl:$PERL5LIB
-braker.pl \
+singularity exec gpfs01/home/mbzlld/software_bin/singularity/braker3.sif braker.pl \
   --AUGUSTUS_ab_initio \
   --threads 16 \
-  --PROTHINT_PATH=/gpfs01/home/mbzlld/software_bin/ProtHint/bin \
-  --genome=../ONTasm.bp.p_ctg_Scleractinia_38-41GC_180-300X_100kb_polished_1.fasta \
+  --genome=/gpfs01/home/mbzlld/data/ctenella/ctenella_chagius_asm.fasta \
   --species=Ctenella_chagius \
-  --prot_seq=training_proteins/cnidaria_proteins.faa.gz
-conda deactivate
+  --prot_seq=training_proteins/cnidaria_proteins.faa.gz \
+  --gff3
+
+
+#  --PROTHINT_PATH=/gpfs01/home/mbzlld/software_bin/ProtHint/bin \
+
+module unload singularity/3.8.5
+
 
 # not running this block because braker3 runs augustus anyway
 ###  # run augustus to annotate the genome assembly
