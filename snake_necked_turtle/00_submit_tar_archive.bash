f1009cf799fa6497176688b8056180c49a9d83f2
Laura Dean <lldean18@gmail.com>
2026-06-16 14:13:13 +0100

    adding script to submit the tar archive without pwd

---
+++ snake_necked_turtle/00_submit_tar_archive.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+# 16/6/26
+
+# script to submit the pod5 tar archive for the snake-necked turtle to the ENA
+
+#SBATCH --partition=defq
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=30g
+#SBATCH --time=100:00:00
+#SBATCH --job-name=ENA_submission
+#SBATCH --output=/gpfs01/home/mbzlld/code_and_scripts/slurm_out_scripts/slurm-%x-%j.out
+
+
+# make ENA submission
+curl --upload-file /gpfs01/home/mbzlld/data/OrgOne/turtle/chelodina_mccordi_mccordi_pod5s.tar.gz --user Webin-154:******** ftp://webin2.ebi.ac.uk
+
+
+
