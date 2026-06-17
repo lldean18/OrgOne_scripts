3da2f6fce542b189b6c9fddeec87773fb08fd68e
Laura Dean <lldean18@gmail.com>
2026-06-17 16:38:34 +0100

    running for vcfs

--- ctenella/the_twelve/00_backup_to_sharepoint.bash
+++ ctenella/the_twelve/00_backup_to_sharepoint.bash
@@ -6,7 +6,7 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=40g
 #SBATCH --time=24:00:00
-#SBATCH --job-name=rclone_ctenella_bams
+#SBATCH --job-name=rclone_ctenella
 #SBATCH --output=/gpfs01/home/mbzlld/code_and_scripts/slurm_out_scripts/slurm-%x-%j.out
 
 # load the software
@@ -14,10 +14,11 @@ module load rclone-uon/1.65.2
 
 # copy a directory called cats from the HPC to MySite
 #rclone --transfers 4 --checkers 4 --bwlimit 100M --checksum copy /gpfs01/home/mbzlld/data/ctenella/the_twelve/bams ctenella:ctenella_bams
-rclone --transfers 4 --checkers 4 --bwlimit 100M --checksum copy /gpfs01/home/mbzlld/data/ctenella/assembly ctenella:assembly
+#rclone --transfers 4 --checkers 4 --bwlimit 100M --checksum copy /gpfs01/home/mbzlld/data/ctenella/assembly ctenella:assembly
+rclone --transfers 4 --checkers 4 --bwlimit 100M --checksum --max-depth 1 copy /gpfs01/home/mbzlld/data/ctenella/the_twelve/variants ctenella:variants
 
 # Check the directory has copied successfully
-rclone check --one-way /gpfs01/home/mbzlld/data/ctenella/assembly ctenella:assembly
+rclone check --one-way --max-depth 1 /gpfs01/home/mbzlld/data/ctenella/the_twelve/variants ctenella:variants
 
 # unload the software
 module unload rclone-uon/1.65.2
