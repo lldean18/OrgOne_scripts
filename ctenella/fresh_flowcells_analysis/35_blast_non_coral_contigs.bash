63cf438da10b2cfdbc0c57e56bf5755145717f6c
Laura Dean <lldean18@gmail.com>
2026-06-19 15:08:30 +0100

    increasing to max run time too

--- ctenella/fresh_flowcells_analysis/35_blast_non_coral_contigs.bash
+++ ctenella/fresh_flowcells_analysis/35_blast_non_coral_contigs.bash
@@ -9,7 +9,6 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=32
 #SBATCH --mem=10g
-#SBATCH --time=40:00:00
 #SBATCH --output=/gpfs01/home/mbzlld/code_and_scripts/slurm_out_scripts/slurm-%x-%j.out
 
 
