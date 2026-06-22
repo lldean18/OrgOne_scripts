c6c0003490d078f52e68de89a1191cad9f737110
Laura Dean <lldean18@gmail.com>
2026-06-22 10:49:30 +0100

    updating and running again as somehow the timelimit disappeared

--- ctenella/fresh_flowcells_analysis/35_blast_non_coral_contigs.bash
+++ ctenella/fresh_flowcells_analysis/35_blast_non_coral_contigs.bash
@@ -9,6 +9,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=32
 #SBATCH --mem=80g
+#SBATCH -time=180:00:00
 #SBATCH --output=/gpfs01/home/mbzlld/code_and_scripts/slurm_out_scripts/slurm-%x-%j.out
 
 
