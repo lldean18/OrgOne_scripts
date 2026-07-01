fd99644147bec0a93b02c38c3f8d731e6195eb48
Laura Dean <lldean18@gmail.com>
2026-07-01 15:23:27 +0100

    decreasing run time so it would start as nodes are going down for maintenence on Mon

--- ctenella/fresh_flowcells_analysis/33_annotate_genome.bash
+++ ctenella/fresh_flowcells_analysis/33_annotate_genome.bash
@@ -9,7 +9,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=17
 #SBATCH --mem=360g
-#SBATCH --time=168:00:00
+#SBATCH --time=100:00:00
 #SBATCH --output=/gpfs01/home/mbzlld/code_and_scripts/slurm_out_scripts/slurm-%x-%j.out
 
 
